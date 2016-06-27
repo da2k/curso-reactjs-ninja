f6ac5c2246142acadbcc2f91a969b8590a9195de
Fernando Daciuk <f.daciuk@gmail.com>
2016-06-26 21:33:16 -0300

    Create react components

--- examples/react-webpack/src/app.js
+++ examples/react-webpack/src/app.js
@@ -1,7 +1,11 @@
 'use strict'
 
-function sum (val1, val2) {
-  return val1 + val2
-}
+var React = require('react')
 
-module.exports = sum
+var Title = React.createClass({
+  render: function () {
+    return React.createElement('h1', null, 'Título')
+  }
+})
+
+module.exports = Title
--- examples/react-webpack/src/index.js
+++ examples/react-webpack/src/index.js
@@ -1,5 +1,10 @@
 'use strict'
 
-var sum = require('./app')
+var React = require('react')
+var ReactDOM = require('react-dom')
+var Title = require('./app')
 
-console.log(sum(1, 2))
+ReactDOM.render(
+  React.createElement(Title),
+  document.querySelector('[data-js="app"]')
+)
