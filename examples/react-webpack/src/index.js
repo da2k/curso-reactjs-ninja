1d9894a12bf6641bafc503c118ee4812fb986cc3
Fernando Daciuk <f.daciuk@gmail.com>
2016-07-03 14:30:41 -0300

    Update index.js

--- examples/react-webpack/src/index.js
+++ examples/react-webpack/src/index.js
@@ -1,21 +1,20 @@
 'use strict'
 
 import React from 'react'
-import { AppContainer } from 'react-hot-loader'
 import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 import App from './app'
 
-const renderApp = (NewApp) => {
+const renderApp = (NextApp) => {
   render(
     <AppContainer>
-      <NewApp />
+      <NextApp />
     </AppContainer>,
     document.querySelector('[data-js="app"]')
   )
 }
 
 renderApp(App)
-
 
 if (module.hot) {
   module.hot.accept('./app', () => {
