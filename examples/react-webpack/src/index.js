2641dd06b352a45d46b8ea225f1e6c9b69d5343f
Fernando Daciuk <f.daciuk@gmail.com>
2016-07-02 22:38:56 -0300

    Update index.js to use hot reloader

--- examples/react-webpack/src/index.js
+++ examples/react-webpack/src/index.js
@@ -1,10 +1,25 @@
 'use strict'
 
 import React from 'react'
+import { AppContainer } from 'react-hot-loader'
 import { render } from 'react-dom'
-import Title from './app'
+import App from './app'
 
-render(
-  <Title />,
-  document.querySelector('[data-js="app"]')
-)
+const renderApp = (NewApp) => {
+  render(
+    <AppContainer>
+      <NewApp />
+    </AppContainer>,
+    document.querySelector('[data-js="app"]')
+  )
+}
+
+renderApp(App)
+
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    const NextApp = require('./app').default
+    renderApp(NextApp)
+  })
+}
