f585a5fce683f66b75db553dd79af8bd9e074ab3
Fernando Daciuk <f.daciuk@gmail.com>
2016-07-02 22:38:00 -0300

    Add new component

---
+++ examples/react-webpack/src/new-component.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import React from 'react'
+
+const NewComponent = React.createClass({
+  render: function () {
+    return <div>New component</div>
+  }
+})
+
+export default NewComponent
