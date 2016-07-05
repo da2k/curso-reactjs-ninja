cf018ed418b5f213f781a0a1f69a562d0db058db
Fernando Daciuk <f.daciuk@gmail.com>
2016-07-04 22:21:00 -0300

    Use Component instead createClass

--- examples/m01/learning-props/src/app.js
+++ examples/m01/learning-props/src/app.js
@@ -1,13 +1,16 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
+import Title from './title'
 
-const App = React.createClass({
-  render: function () {
+class App extends Component {
+  render () {
     return (
-      <div>My App</div>
+      <div className='container'>
+        <Title name='Fernando' />
+      </div>
     )
   }
-})
+}
 
 export default App
