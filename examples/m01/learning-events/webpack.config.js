34ee8516e71f3525f1fcfbe15a6e7c5b297d17ac
Fernando Daciuk <f.daciuk@gmail.com>
2016-07-09 21:24:12 -0300

    Learning events

---
+++ examples/m01/learning-events/webpack.config.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const path = require('path')
+const webpack = require('webpack')
+const validate = require('webpack-validator')
+
+module.exports = validate({
+  devtool: 'source-map',
+
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, 'src', 'index')
+  ],
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      include: /src/,
+      loader: 'standard'
+    }],
+
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      include: /src/,
+      loader: 'babel'
+    }]
+  }
+})
