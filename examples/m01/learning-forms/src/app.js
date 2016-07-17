fdd82d79ed4585d5ac4214930b599ef23a509007
Fernando Daciuk <f.daciuk@gmail.com>
2016-07-17 18:34:53 -0300

    Ad checkbox and radio examples

--- examples/m01/learning-forms/src/app.js
+++ examples/m01/learning-forms/src/app.js
@@ -6,18 +6,29 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      value: 'Valor inicial'
+      value: 'Valor inicial',
+      checked: false
     }
   }
+
   render () {
     return (
-      <div>
-        <form>
-          <input type='text' value={this.state.value} onChange={(e) => {
-            this.setState({ value: e.target.value })
-          }} />
-        </form>
-      </div>
+      <form>
+        <input type='text' value={this.state.value} onChange={(e) => {
+          this.setState({ value: e.target.value })
+        }} />
+
+        <label>
+          <input
+            type='checkbox'
+            value='my-checkbox'
+            defaultChecked />
+          Checkbox
+        </label>
+
+        <input type='radio' name='rd' value='1' defaultChecked /> Radio 1
+        <input type='radio' name='rd' value='2' /> Radio 2
+      </form>
     )
   }
 }
