f4c3c0b115017e9a45e70510d447329dd66c511c
Fernando Daciuk <f.daciuk@gmail.com>
2016-07-17 12:20:10 -0300

    Add another methods on timer component

--- examples/m01/learning-state/src/timer.js
+++ examples/m01/learning-state/src/timer.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 
 class Timer extends Component {
   constructor () {
+    console.log('constructor timer')
     super()
     this.state = {
       time: 0
@@ -13,25 +14,24 @@ class Timer extends Component {
   }
 
   componentWillReceiveProps (nextProps) {
-    console.log('componentWillReceiveProps', this.props, nextProps)
+    console.log('componentWillReceiveProps timer', this.props, nextProps)
   }
 
   shouldComponentUpdate (nextProps, nextState) {
-    // console.log('shouldComponentUpdate', this.props, nextProps)
+    // console.log('shouldComponentUpdate timer', this.props, nextProps)
     return this.props.time !== nextProps.time
   }
 
   componentWillUpdate (nextProps, nextState) {
-    alert('componentWillUpdate')
-    console.log('componentWillUpdate')
+    console.log('componentWillUpdate timer', this.props, nextProps)
   }
 
   componentDidUpdate (prevProps, prevState) {
-    alert('componentDidUpdate')
-    console.log('componentDidUpdate')
+    console.log('componentDidUpdate timer', this.props, prevProps)
   }
 
   componentDidMount () {
+    console.log('componentDidMount timer')
     this.timer = setInterval(() => {
       this.setState({
         time: this.state.time + 1
@@ -40,10 +40,12 @@ class Timer extends Component {
   }
 
   componentWillUnmount () {
+    console.log('componentWillUnmount timer')
     clearInterval(this.timer)
   }
 
   render () {
+    console.log('render timer')
     return <div>Timer: {this.state.time}</div>
   }
 }
