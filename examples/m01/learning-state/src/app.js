9e36356008935d9a80f208794315e9a7cfafb02c
Fernando Daciuk <f.daciuk@gmail.com>
2016-07-17 12:19:42 -0300

    Add console.log on methods for app component

--- examples/m01/learning-state/src/app.js
+++ examples/m01/learning-state/src/app.js
@@ -5,7 +5,7 @@ import Timer from './timer'
 
 class App extends Component {
   constructor () {
-    console.log('constructor')
+    console.log('constructor app')
     super()
     this.state = {
       time: 0,
@@ -14,15 +14,15 @@ class App extends Component {
   }
 
   componentWillMount () {
-    console.log('componentWillMount')
+    console.log('componentWillMount app')
   }
 
   componentDidMount () {
-    console.log('componentDidMount')
+    console.log('componentDidMount app')
   }
 
   render () {
-    console.log('render')
+    console.log('render app')
 
     return (
       <div>
