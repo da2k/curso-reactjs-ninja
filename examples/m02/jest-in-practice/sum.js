86fdf60a7550e9ac960ae426a743ebd521181acd
Fernando Daciuk <f.daciuk@gmail.com>
2016-10-02 16:46:00 -0300

    Add es6 example

--- examples/m02/jest-in-practice/sum.js
+++ examples/m02/jest-in-practice/sum.js
@@ -1,4 +1,4 @@
-module.exports = () => {
+export default () => {
   if (1 == 2) {
     return
   }
--- examples/m02/jest-in-practice/sum.test.js
+++ examples/m02/jest-in-practice/sum.test.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const expect = require('chai').expect
-const sum = require('./sum')
+import { expect } from 'chai'
+import sum from './sum'
 
 it('sum should be a function', () => {
   expect(sum).to.be.a('function')
