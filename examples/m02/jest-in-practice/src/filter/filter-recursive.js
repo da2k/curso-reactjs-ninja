fafed04c6eaf17bca0bf7f025ed1b4f6c43f2375
Fernando Daciuk <f.daciuk@gmail.com>
2016-10-08 20:16:31 -0300

    Add filter recursive

---
+++ examples/m02/jest-in-practice/src/filter/filter-recursive.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const filter = (arr, func) => {
+  return (function filterInternal(arrayInternal, counter) {
+    const [head, ...tail] = arrayInternal
+
+    return arrayInternal.length === 0
+      ? []
+      : (func(head, counter, arr) ? [head] : []).concat(filterInternal(tail, counter + 1))
+  })(arr, 0)
+}
+
+export default filter
