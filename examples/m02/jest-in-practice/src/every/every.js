a6599ca9e0f6ff3f4df0b89e01f1955d3fe23768
Fernando Daciuk <f.daciuk@gmail.com>
2016-10-09 17:57:07 -0300

    Add every method

---
+++ examples/m02/jest-in-practice/src/every/every.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const every = (arr, func) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (!func(arr[i])) {
+      return false
+    }
+  }
+
+  return true
+}
+
+export default every
---
+++ examples/m02/jest-in-practice/src/every/every.test.js
@@ -0,0 +1,20 @@
+'use strict'
+
+import { expect } from 'chai'
+import every from './every'
+
+it('every should be a function', () => {
+  expect(every).to.be.a('function')
+})
+
+it('every([], (item) => item) should return true', () => {
+  expect(every([], (item) => item)).to.be.ok
+})
+
+it('every([0, 2, 3], (item) => item) should return false', () => {
+  expect(every([0, 2, 3], (item) => item)).to.not.be.ok
+})
+
+it('every([1, 2, 3], (item) => item) should return true', () => {
+  expect(every([1, 2, 3], (item) => item)).to.be.ok
+})
