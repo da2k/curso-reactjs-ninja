affbbdda6852b13a66a0757bec09a974e7709392
Fernando Daciuk <f.daciuk@gmail.com>
2016-10-23 12:41:42 -0200

    Add reduce recursive

---
+++ examples/m02/jest-in-practice/src/reduce/reduce-recursive.test.js
@@ -0,0 +1,51 @@
+'use strict'
+
+import { expect } from 'chai'
+import reduce from './reduce-recursive'
+
+it('reduce should be a function', () => {
+  expect(reduce).to.be.a('function')
+})
+
+it('reduce([1, 2, 3], (acc, item) => acc + item, 0) should return 6', () => {
+  const before = reduce([1, 2, 3], (acc, item) => acc + item, 0)
+  const after = 6
+  expect(before).to.be.equal(after)
+})
+
+it('reduce([2, 3, 4], (acc, item) => acc + item, 0) should return 9', () => {
+  const before = reduce([2, 3, 4], (acc, item) => acc + item, 0)
+  const after = 9
+  expect(before).to.be.equal(after)
+})
+
+it('reduce([2, 3, 4], (acc, item) => acc + item) should return 9', () => {
+  const before = reduce([2, 3, 4], (acc, item) => acc + item)
+  const after = 9
+  expect(before).to.be.equal(after)
+})
+
+it('reduce([1, 2], (acc, item) => { acc["number-" + item] = item; return acc }, {}) should return {"number-1": 1, "number-2": 2}', () => {
+  const before = reduce([1, 2], (acc, item) => {
+    acc["number-" + item] = item
+    return acc
+  }, {})
+  const after = {"number-1": 1, "number-2": 2}
+  expect(before).to.be.deep.equal(after)
+})
+
+it('reduce([1, 2], (acc, item, index) => acc + index, 0) should return 1', () => {
+  const before = reduce([1, 2], (acc, item, index) => acc + index, 0)
+  const after = 1
+  expect(before).to.be.equal(after)
+})
+
+it('reduce([1, 2], (acc, item, index, array) => acc + array[index], 0) should return 3', () => {
+  const before = reduce([1, 2], (acc, item, index, array) => acc + array[index], 0)
+  const after = 3
+  expect(before).to.be.equal(after)
+})
+
+
+
+
