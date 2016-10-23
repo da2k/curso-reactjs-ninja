16e7406905d6f4c00ebe64d254401a48819c9d85
Fernando Daciuk <f.daciuk@gmail.com>
2016-10-23 15:22:32 -0200

    Add find method

---
+++ examples/m02/jest-in-practice/src/find/find.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const find = (arr, func) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i], i, arr)) {
+      return arr[i]
+    }
+  }
+}
+
+export default find
---
+++ examples/m02/jest-in-practice/src/find/find.test.js
@@ -0,0 +1,24 @@
+'use strict'
+
+import { expect } from 'chai'
+import find from './find'
+
+it('find should be a function', () => {
+  expect(find).to.be.a('function')
+})
+
+it('find([1, 2, 3], (item) => item === 1) should return 1', () => {
+  expect(find([1, 2, 3], (item) => item === 1)).to.be.equal(1)
+})
+
+it('find([1, 2, 3], (item) => item === 3) should return 3', () => {
+  expect(find([1, 2, 3], (item) => item === 3)).to.be.equal(3)
+})
+
+it('find([1, 2, 3], (item, index) => index === 1) should return 2', () => {
+  expect(find([1, 2, 3], (item, index) => index === 1)).to.be.equal(2)
+})
+
+it('find([1, 2, 3], (item, index, array) => array[index] === 2) should return 2', () => {
+  expect(find([1, 2, 3], (item, index, array) => array[index] === 2)).to.be.equal(2)
+})
