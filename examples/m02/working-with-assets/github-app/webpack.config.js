58255d7626616596f034cc087bac3b0002988ae1
Fernando Daciuk <f.daciuk@gmail.com>
2016-11-02 16:13:33 -0200

    Add extract text plugin and change raw loader to css loader

--- examples/m02/working-with-assets/github-app/webpack.config.js
+++ examples/m02/working-with-assets/github-app/webpack.config.js
@@ -5,6 +5,7 @@ const webpack = require('webpack')
 const validate = require('webpack-validator')
 
 const HtmlPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = validate({
   devtool: 'source-map',
@@ -24,6 +25,7 @@ module.exports = validate({
 
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin('[name]-[hash].css'),
     new HtmlPlugin({
       title: 'GitHub app',
       template: path.join(__dirname, 'src', 'html', 'template.html')
@@ -47,7 +49,7 @@ module.exports = validate({
       test: /\.css$/,
       exclude: /node_modules/,
       include: /src/,
-      loaders: ['style', 'raw']
+      loader: ExtractTextPlugin.extract('style', 'css')
     }]
   }
 })
