89befb808a3e2344f25d28d5c1986667791a165f
Fernando Daciuk <f.daciuk@gmail.com>
2016-11-03 22:03:38 -0200

    Add webpack dashboard

--- examples/m02/working-with-assets/github-app/webpack.config.js
+++ examples/m02/working-with-assets/github-app/webpack.config.js
@@ -6,6 +6,7 @@ const validate = require('webpack-validator')
 
 const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const DashboardPlugin = require('webpack-dashboard/plugin')
 
 module.exports = validate({
   devtool: 'source-map',
@@ -26,6 +27,8 @@ module.exports = validate({
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new ExtractTextPlugin('[name]-[hash].css'),
+    new DashboardPlugin(),
+
     new HtmlPlugin({
       title: 'GitHub app',
       template: path.join(__dirname, 'src', 'html', 'template.html')
