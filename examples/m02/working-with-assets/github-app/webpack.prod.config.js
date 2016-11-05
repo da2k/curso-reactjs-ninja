b0df09c93f3e2ee9c1b7ff15a8c5d2d13e40a830
Fernando Daciuk <f.daciuk@gmail.com>
2016-11-05 20:21:07 -0200

    Separate critical rendering path files

--- examples/m02/working-with-assets/github-app/webpack.prod.config.js
+++ examples/m02/working-with-assets/github-app/webpack.prod.config.js
@@ -7,6 +7,9 @@ const validate = require('webpack-validator')
 const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
+const crp = new ExtractTextPlugin('crp.css')
+const styles = new ExtractTextPlugin('[name]-[hash].css')
+
 module.exports = validate({
   entry: path.join(__dirname, 'src', 'index'),
 
@@ -16,7 +19,8 @@ module.exports = validate({
   },
 
   plugins: [
-    new ExtractTextPlugin('[name]-[hash].css'),
+    crp,
+    styles,
 
     new webpack.DefinePlugin({
       'process.env': {
@@ -53,9 +57,14 @@ module.exports = validate({
       loader: 'babel'
     }, {
       test: /\.css$/,
+      exclude: /node_modules|(search|style)\.css/,
+      include: /src/,
+      loader: styles.extract('style', 'css')
+    }, {
+      test: /(search|style)\.css$/,
       exclude: /node_modules/,
       include: /src/,
-      loader: ExtractTextPlugin.extract('style', 'css')
+      loader: crp.extract('style', 'css')
     }]
   }
 })
