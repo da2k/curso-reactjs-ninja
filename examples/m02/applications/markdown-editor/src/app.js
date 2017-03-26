088e133b072b82334c2e1ba7b82ae9fa6ee53445
Fernando Daciuk <f.daciuk@gmail.com>
2017-03-26 20:57:30 -0300

    Add marked

--- examples/m02/applications/markdown-editor/src/app.js
+++ examples/m02/applications/markdown-editor/src/app.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import React, { Component } from 'react'
+import marked from 'marked'
 import MarkdownEditor from './markdown-editor'
 
 import 'normalize.css'
@@ -16,7 +17,7 @@ class App extends Component {
     }
 
     this.getMarkup = () => {
-      return { __html: this.state.value }
+      return { __html: marked(this.state.value) }
     }
   }
 
