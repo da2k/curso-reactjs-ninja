ba0213cf995a87158763778823ba6aaa652a658c
Fernando Daciuk <f.daciuk@gmail.com>
2017-04-01 20:05:29 -0300

    Order files on html plugin and improve analyzer call

--- examples/m02/applications/markdown-editor/webpack/prod.config.js
+++ examples/m02/applications/markdown-editor/webpack/prod.config.js
@@ -14,8 +14,6 @@ module.exports = {
   output: common.output,
 
   plugins: [
-    new BundleAnalyzerPlugin(),
-
     new CleanPlugin(['dist'], {
       root: common.paths.root
     }),
@@ -46,12 +44,23 @@ module.exports = {
       )
     }),
 
-    new HtmlPlugin(common.htmlPluginConfig),
+    new HtmlPlugin(Object.assign({}, common.htmlPluginConfig, {
+      minify: { collapseWhitespace: true },
+
+      chunksSortMode: (chunk1, chunk2) => {
+        const order = ['react-build', 'vendor', 'main']
+        const left = order.indexOf(chunk1.names[0])
+        const right = order.indexOf(chunk2.names[0])
+        return left - right
+      }
+    })),
 
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true
     })
-  ],
+  ].concat(
+    process.env.ANALYZER ? new BundleAnalyzerPlugin() : []
+  ),
 
   module: {
     rules: [
