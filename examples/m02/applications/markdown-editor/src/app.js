f839210e5bd0ab7dc1e148c0a4b67bdd9e096596
Fernando Daciuk <f.daciuk@gmail.com>
2017-04-23 19:13:01 -0300

    Add dynamic entries on localStorage with node-uuid

--- examples/m02/applications/markdown-editor/src/app.js
+++ examples/m02/applications/markdown-editor/src/app.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import React, { Component } from 'react'
+import { v4 } from 'node-uuid'
 import marked from 'marked'
 import MarkdownEditor from 'views/markdown-editor'
 
@@ -20,8 +21,14 @@ import('highlight.js').then((hljs) => {
 class App extends Component {
   constructor () {
     super()
-    this.state = {
+
+    this.clearState = () => ({
       value: '',
+      id: v4()
+    })
+
+    this.state = {
+      ...this.clearState(),
       isSaving: null
     }
 
@@ -38,29 +45,28 @@ class App extends Component {
 
     this.handleSave = (value) => {
       if (this.state.isSaving) {
-        localStorage.setItem('md', this.state.value)
+        localStorage.setItem(this.state.id, this.state.value)
         this.setState({ isSaving: false })
       }
     }
 
+    this.createNew = () => {
+      this.setState(this.clearState())
+      this.textarea.focus()
+    }
+
     this.handleRemove = () => {
-      localStorage.removeItem('md')
-      this.setState({ value: '' })
+      localStorage.removeItem(this.state.id)
+      this.createNew()
     }
 
     this.handleCreate = () => {
-      this.setState({ value: '' })
-      this.textarea.focus()
+      this.createNew()
     }
 
     this.textareaRef = (node) => {
       this.textarea = node
     }
-  }
-
-  componentDidMount () {
-    const value = localStorage.getItem('md')
-    this.setState({ value: value || '' })
   }
 
   componentDidUpdate () {
