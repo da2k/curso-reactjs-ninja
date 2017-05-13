055f9ea4cea8611672fef60f6a756f4ef879cb67
Fernando Daciuk <f.daciuk@gmail.com>
2017-05-13 19:29:47 -0300

    Clean app to show context api examples

--- examples/m02/context-api/src/app.js
+++ examples/m02/context-api/src/app.js
@@ -5,35 +5,10 @@ import React, { PureComponent } from 'react'
 import './css/style.css'
 
 class App extends PureComponent {
-  constructor () {
-    super()
-    this.state = {
-      title: '...',
-      Component: 'div'
-    }
-  }
-
-  getTitle () {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve('My app with async / await!')
-      }, 2000)
-    })
-  }
-
-  async componentDidMount () {
-    const title = await import('components/title')
-
-    this.setState({
-      title: await this.getTitle(),
-      Component: title.default
-    })
-  }
-
   render () {
     return (
       <div>
-        <this.state.Component>{this.state.title}</this.state.Component>
+        App
       </div>
     )
   }
