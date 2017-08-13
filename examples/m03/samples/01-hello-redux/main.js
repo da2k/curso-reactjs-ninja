9f77e9715cf50ddc6fd43207bf4322588c0d7b60
Fernando Daciuk <f.daciuk@gmail.com>
2017-08-13 17:41:48 -0300

    Add first redux example - create reducer

---
+++ examples/m03/samples/01-hello-redux/main.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT': return state + 1
+    case 'DECREMENT': return state - 1
+  }
+
+  return state
+}
+
+console.assert(
+  counter(0, { type: 'INCREMENT' }) === 1
+)
+
+console.assert(
+  counter(1, { type: 'INCREMENT' }) === 2
+)
+
+console.assert(
+  counter(2, { type: 'DECREMENT' }) === 1
+)
+
+console.assert(
+  counter(5, { type: 'DECREMENT' }) === 4
+)
+
+console.assert(
+  counter(3, { type: 'SOMETHING' }) === 3
+)
+
+console.assert(
+  counter(undefined, {}) === 0
+)
+
+
