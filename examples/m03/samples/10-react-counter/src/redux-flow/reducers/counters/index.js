3f30ae574ce12c94c2f3f8b9f7fc91a60b0c5f47
Fernando Daciuk <f.daciuk@gmail.com>
2017-08-20 20:42:30 -0300

    Refactory for tests

--- examples/m03/samples/10-react-counter/src/redux-flow/reducers/counters/index.js
+++ examples/m03/samples/10-react-counter/src/redux-flow/reducers/counters/index.js
@@ -1,17 +1,26 @@
 'use strict'
 
-export default (state, action) => {
+export const ADD_COUNTER = 'ADD_COUNTER'
+export const REMOVE_COUNTER = 'REMOVE_COUNTER'
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const initialState = []
+
+export default (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_COUNTER':
+    case ADD_COUNTER:
       return state.concat(0)
 
-    case 'REMOVE_COUNTER':
+    case REMOVE_COUNTER:
       return state.filter((_, index) => index !== action.index)
 
-    case 'INCREMENT':
+    case INCREMENT:
       return state.map((c, i) => i === action.index ? c + 1 : c)
 
-    case 'DECREMENT':
+    case DECREMENT:
       return state.map((c, i) => i === action.index ? c - 1 : c)
   }
+
+  return state
 }
