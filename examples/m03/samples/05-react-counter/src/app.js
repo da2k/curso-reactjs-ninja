3ea02ad8747a88d7574d8a5d04b62740cc44bd81
Fernando Daciuk <f.daciuk@gmail.com>
2017-08-20 16:19:25 -0300

    Add counter in React

--- examples/m03/samples/05-react-counter/src/app.js
+++ examples/m03/samples/05-react-counter/src/app.js
@@ -1,42 +1,10 @@
 'use strict'
 
-import React, { PureComponent } from 'react'
+import React from 'react'
+import Counter from 'components/counter'
 
-import './css/style.css'
-
-class App extends PureComponent {
-  constructor () {
-    super()
-    this.state = {
-      title: '...',
-      Component: 'div'
-    }
-  }
-
-  getTitle () {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve('My app with async / await!')
-      }, 2000)
-    })
-  }
-
-  async componentDidMount () {
-    const title = await import('components/title')
-
-    this.setState({
-      title: await this.getTitle(),
-      Component: title.default
-    })
-  }
-
-  render () {
-    return (
-      <div>
-        <this.state.Component>{this.state.title}</this.state.Component>
-      </div>
-    )
-  }
-}
+const App = () => (
+  <Counter />
+)
 
 export default App
