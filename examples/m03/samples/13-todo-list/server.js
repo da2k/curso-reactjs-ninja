9417194c046aa43cf84e5c4c600bdfdf2a6a172c
Fernando Daciuk <f.daciuk@gmail.com>
2017-09-17 19:26:00 -0300

    Todo List: Add overlay on webpack dev server

--- examples/m03/samples/13-todo-list/server.js
+++ examples/m03/samples/13-todo-list/server.js
@@ -8,6 +8,7 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true,
+  overlay: true,
   stats: { colors: true }
 }).listen(3000, (err) => {
   if (err) {
