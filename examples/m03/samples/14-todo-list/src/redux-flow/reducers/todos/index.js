c0ac817fcda1db7e393e498e7eb3092f2371a6e2
Fernando Daciuk <f.daciuk@gmail.com>
2017-09-17 20:42:22 -0300

    Todo List: add tests to toggle todo

--- examples/m03/samples/14-todo-list/src/redux-flow/reducers/todos/index.js
+++ examples/m03/samples/14-todo-list/src/redux-flow/reducers/todos/index.js
@@ -1,7 +1,31 @@
 'use strict'
 
-const todos = () => {
-  return [{ id: 0, text: 'Hey', completed: false }]
+import { ADD_TODO, TOGGLE_TODO } from './actions'
+
+const initialState = []
+
+const todos = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat({
+        id: action.payload.id,
+        text: action.payload.text,
+        completed: false
+      })
+
+    case TOGGLE_TODO:
+      return state.map((todo) => {
+        if (todo.id !== action.payload.id) {
+          return todo
+        }
+
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+      })
+
+  }
 }
 
 export default todos
--- examples/m03/samples/14-todo-list/src/redux-flow/reducers/todos/todos.test.js
+++ examples/m03/samples/14-todo-list/src/redux-flow/reducers/todos/todos.test.js
@@ -3,7 +3,7 @@
 import { expect } from 'chai'
 import deepFreeze from 'deep-freeze'
 import todos from './index'
-import { ADD_TODO } from './actions'
+import { ADD_TODO, TOGGLE_TODO } from './actions'
 
 it('should todos be a function', () => {
   expect(todos).to.be.a('function')
@@ -19,3 +19,45 @@ it('should add a todo item', () => {
 
   expect(todos(before, action)).to.be.deep.equal(after)
 })
+
+it('should add a new todo item', () => {
+  const before = deepFreeze([
+    { id: 0, text: 'Hey', completed: false }
+  ])
+
+  const action = deepFreeze({
+    type: ADD_TODO,
+    payload: { id: 1, text: 'Ho' }
+  })
+
+  const after = [
+    { id: 0, text: 'Hey', completed: false },
+    { id: 1, text: 'Ho', completed: false }
+  ]
+
+  expect(todos(before, action)).to.be.deep.equal(after)
+})
+
+it('should toggle first todo', () => {
+  const before = deepFreeze([
+    { id: 0, text: 'Hey', completed: false },
+    { id: 1, text: 'Ho', completed: false }
+  ])
+
+  const action = deepFreeze({
+    type: TOGGLE_TODO,
+    payload: { id: 0 }
+  })
+
+  const after = [
+    { id: 0, text: 'Hey', completed: true },
+    { id: 1, text: 'Ho', completed: false }
+  ]
+
+  expect(todos(before, action)).to.be.deep.equal(after)
+})
+
+
+
+
+
