dcc5edc218cb5dc172643eda79d1e2248e0985f6
Fernando Daciuk <f.daciuk@gmail.com>
2017-10-14 19:11:23 -0300

    Todo List: ADD_TODO, uuid

--- examples/m03/samples/17-todo-list/src/app.js
+++ examples/m03/samples/17-todo-list/src/app.js
@@ -1,10 +1,17 @@
 'use strict'
 
 import React from 'react'
+import { connect } from 'react-redux'
+import { addTodo } from 'reducers/todos/action-creators'
 
-const App = () => (
+const App = ({ todos, handleAddTodo }) => (
   <div>
-    <input type='text' />
+    <form onSubmit={handleAddTodo}>
+      <input type='text' name='todo' />
+      <button type='submit'>Adicionar</button>
+    </form>
+
+    {console.log(todos)}
 
     <ul>
       <li style={{ textDecoration: 'line-through' }}>Item 1</li>
@@ -21,4 +28,15 @@ const App = () => (
   </div>
 )
 
-export default App
+const mapStateToProps = (state) => ({
+  todos: state
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  handleAddTodo: (e) => {
+    e.preventDefault()
+    dispatch(addTodo(e.target.todo.value))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
