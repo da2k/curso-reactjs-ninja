799fd084f311167708e7f8754b46dae520ebcf67
Fernando Daciuk <f.daciuk@gmail.com>
2017-10-14 19:39:11 -0300

    Todo List: Add 19

---
+++ examples/m03/samples/19-todo-list/src/index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './app'
+import reducer from 'reducers/todos'
+
+const store = createStore(reducer)
+
+const renderApp = (NextApp) => {
+  render(
+    <AppContainer>
+      <Provider store={store}>
+        <NextApp />
+      </Provider>
+    </AppContainer>,
+    document.querySelector('[data-js="app"]')
+  )
+}
+
+renderApp(App)
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    const NextApp = require('./app').default
+    renderApp(NextApp)
+  })
+}
