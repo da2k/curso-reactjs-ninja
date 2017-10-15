2912f619c74ad1357e6d3f74b4735c49f30bcef5
Fernando Daciuk <f.daciuk@gmail.com>
2017-10-15 20:47:09 -0200

    Todo List: Redux - combineReducers

--- examples/m03/samples/22-todo-list/src/redux-flow/reducers/index.js
+++ examples/m03/samples/22-todo-list/src/redux-flow/reducers/index.js
@@ -1,13 +1,10 @@
 'use strict'
 
-import reducerTodos from './todos'
-import reducerVisibilityFilter from './visibility-filter'
+import { combineReducers } from 'redux'
+import todos from './todos'
+import visibilityFilter from './visibility-filter'
 
-const rootReducer = (state = {}, action) => {
-  return {
-    todos: reducerTodos(state.todos, action),
-    visibilityFilter: reducerVisibilityFilter(state.visibilityFilter, action)
-  }
-}
-
-export default rootReducer
+export default combineReducers({
+  todos,
+  visibilityFilter
+})
