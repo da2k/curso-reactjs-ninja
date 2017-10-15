f1ad43bd877a5043742f10970e26f14f4eb764ef
Fernando Daciuk <f.daciuk@gmail.com>
2017-10-15 21:22:35 -0200

    Todo List: Refactory - isolate components

--- examples/m03/samples/24-todo-list/src/app.js
+++ examples/m03/samples/24-todo-list/src/app.js
@@ -1,49 +1,16 @@
 'use strict'
 
 import React from 'react'
-import { connect } from 'react-redux'
-import { addTodo, toggleTodo } from 'reducers/todos/action-creators'
+import Form from 'components/form'
+import TodosList from 'components/todos-list'
+import Filter from 'components/filter'
 
-const App = ({ todos, handleAddTodo, handleToggleTodo }) => (
+const App = () => (
   <div>
-    <form onSubmit={handleAddTodo}>
-      <input type='text' name='todo' />
-      <button type='submit'>Adicionar</button>
-    </form>
-
-    <ul>
-      {todos.map((todo) => (
-        <li
-          key={todo.id}
-          style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
-          onClick={handleToggleTodo(todo.id)}
-        >
-          {todo.text}
-        </li>
-      ))}
-    </ul>
-
-    <div>
-      <h3>Mostrar</h3>
-      <span>Todos</span> | <a href=''>Finalizados</a> | <a href=''>A fazer</a>
-    </div>
+    <Form />
+    <TodosList />
+    <Filter />
   </div>
 )
 
-const mapStateToProps = (state) => ({
-  todos: state.todos
-})
-
-const mapDispatchToProps = (dispatch) => ({
-  handleAddTodo: (e) => {
-    e.preventDefault()
-    dispatch(addTodo(e.target.todo.value))
-    e.target.todo.value = ''
-  },
-
-  handleToggleTodo: (id) => (e) => {
-    dispatch(toggleTodo(id))
-  }
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
---
+++ examples/m03/samples/24-todo-list/src/components/filter/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import React from 'react'
+
+const Filter = () => (
+  <div>
+    <h3>Mostrar</h3>
+    <span>Todos</span> | <a href=''>Finalizados</a> | <a href=''>A fazer</a>
+  </div>
+)
+
+export default Filter
