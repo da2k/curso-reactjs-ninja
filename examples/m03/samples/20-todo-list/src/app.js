dd2010fa42bc528a981d2804a6a0a04202a87466
Fernando Daciuk <f.daciuk@gmail.com>
2017-10-15 15:01:03 -0200

    Todo List: Add 20

---
+++ examples/m03/samples/20-todo-list/src/app.js
@@ -0,0 +1,49 @@
+'use strict'
+
+import React from 'react'
+import { connect } from 'react-redux'
+import { addTodo, toggleTodo } from 'reducers/todos/action-creators'
+
+const App = ({ todos, handleAddTodo, handleToggleTodo }) => (
+  <div>
+    <form onSubmit={handleAddTodo}>
+      <input type='text' name='todo' />
+      <button type='submit'>Adicionar</button>
+    </form>
+
+    <ul>
+      {todos.map((todo) => (
+        <li
+          key={todo.id}
+          style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+          onClick={handleToggleTodo(todo.id)}
+        >
+          {todo.text}
+        </li>
+      ))}
+    </ul>
+
+    <div>
+      <h3>Mostrar</h3>
+      <span>Todos</span> | <a href=''>Finalizados</a> | <a href=''>A fazer</a>
+    </div>
+  </div>
+)
+
+const mapStateToProps = (state) => ({
+  todos: state
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  handleAddTodo: (e) => {
+    e.preventDefault()
+    dispatch(addTodo(e.target.todo.value))
+    e.target.todo.value = ''
+  },
+
+  handleToggleTodo: (id) => (e) => {
+    dispatch(toggleTodo(id))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
---
+++ examples/m03/samples/20-todo-list/src/redux-flow/reducers/todos/actions.js
@@ -0,0 +1,4 @@
+'use strict'
+
+export const ADD_TODO = 'ADD_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
