152787103706a95c66315d855eee908185a69b9d
Fernando Daciuk <f.daciuk@gmail.com>
2017-10-28 17:59:12 -0200

    Todo List: connect filter on state

--- examples/m03/samples/26-todo-list/src/components/filter/index.js
+++ examples/m03/samples/26-todo-list/src/components/filter/index.js
@@ -1,12 +1,32 @@
 'use strict'
 
 import React from 'react'
+import { connect } from 'react-redux'
+import * as actions from 'reducers/visibility-filter/actions'
 
-const Filter = () => (
+const Filter = ({ activeFilter }) => (
   <div>
     <h3>Mostrar</h3>
-    <span>Todos</span> | <a href=''>Finalizados</a> | <a href=''>A fazer</a>
+    {filterItems.map((item) => {
+      if (item.action === activeFilter) {
+        return <span key={item.action} style={{ marginRight : 10 }}>{item.label}</span>
+      }
+
+      return <a href='' key={item.action} style={{ marginRight : 10 }}>{item.label}</a>
+    })}
   </div>
 )
 
-export default Filter
+const filterItems = [
+  { label: 'Todos', action: actions.SHOW_ALL },
+  { label: 'Finalizados', action: actions.SHOW_COMPLETED },
+  { label: 'A fazer', action: actions.SHOW_ACTIVE }
+]
+
+// ['Todos', 'Finalizados', 'A fazer']
+
+const mapStateToProps = (state) => ({
+  activeFilter: state.visibilityFilter
+})
+
+export default connect(mapStateToProps)(Filter)
