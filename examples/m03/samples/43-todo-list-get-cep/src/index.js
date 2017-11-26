03290a73c11be5b4d4189b959e7a5b8a82ed912a
Fernando Daciuk <f.daciuk@gmail.com>
2017-11-26 18:41:12 -0200

    Todo List + CEP: Add 43

---
+++ examples/m03/samples/43-todo-list-get-cep/src/index.js
@@ -0,0 +1,43 @@
+'use strict'
+
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './app'
+import reducer from 'reducers'
+
+const store = createStore(reducer)
+
+const renderState = () => {
+  console.log('state:', store.getState())
+}
+
+store.subscribe(renderState)
+renderState()
+
+const renderApp = (NextApp) => {
+  render(
+    <AppContainer>
+      <Provider store={store}>
+        <NextApp />
+      </Provider>
+    </AppContainer>,
+    document.querySelector('[data-js="app"]')
+  )
+}
+
+renderApp(App)
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    const NextApp = require('./app').default
+    renderApp(NextApp)
+  })
+
+  module.hot.accept('reducers', () => {
+    const nextReducer = require('reducers').default
+    store.replaceReducer(nextReducer)
+  })
+}
