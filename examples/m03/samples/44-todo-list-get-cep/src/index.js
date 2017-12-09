0254e6a037b5637f6f7e0617c11d4c56a924c41d
Fernando Daciuk <f.daciuk@gmail.com>
2017-12-09 19:40:51 -0200

    Todo List + CEP: Add example with two middlewares: logger and thunk

--- examples/m03/samples/44-todo-list-get-cep/src/index.js
+++ examples/m03/samples/44-todo-list-get-cep/src/index.js
@@ -15,7 +15,31 @@ const logger = ({ dispatch, getState }) => (next) => (action) => {
   return nextAction
 }
 
-const store = createStore(reducer, applyMiddleware(logger))
+const thunk = ({ dispatch, getState }) => (next) => (action) => {
+  if (typeof action === 'function') {
+    return action(dispatch, getState)
+  }
+  return next(action)
+}
+
+const store = createStore(reducer, applyMiddleware(logger, thunk))
+
+// -------------------
+store.dispatch(lazyAction())
+function lazyAction () {
+  return (dispatch, getState) => {
+    setTimeout(() => {
+      dispatch({
+        type: 'todos:ADD_TODO',
+        payload: {
+          text: 'Lazy Action',
+          id: '123'
+        }
+      })
+    }, 2000)
+  }
+}
+// -------------------
 
 const renderState = () => {
   console.log('state:', store.getState())
