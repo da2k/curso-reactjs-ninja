94dffd61e42808c7a5a72a6c9c1e5a6baecd1473
Fernando Daciuk <f.daciuk@gmail.com>
2017-12-12 18:23:31 -0200

    Todo List + CEP: Pass request from React component to Redux

--- examples/m03/samples/46-todo-list-get-cep/src/components/search-cep/index.js
+++ examples/m03/samples/46-todo-list-get-cep/src/components/search-cep/index.js
@@ -2,39 +2,25 @@
 
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
-import ajax from '@fdaciuk/ajax'
 import SearchCep from './search-cep'
-import { updateAddress } from 'reducers/address/action-creators'
+import { fetchAddress } from 'reducers/address/action-creators'
 
-class SearchCepContainer extends PureComponent {
-  state = { isFetching: false }
-
-  handleSubmit = async (e) => {
-    e.preventDefault()
-    this.setState({ isFetching: true })
-
-    const cep = e.target.cep.value
-    const response = await ajax().get('http://apps.widenet.com.br/busca-cep/api/cep.json', { code: cep })
-
-    this.setState({ isFetching: false })
-    this.props.updateAddress(response)
-  }
-
-  render () {
-    return (
-      <SearchCep
-        {...this.state}
-        {...this.props.address}
-        handleSubmit={this.handleSubmit}
-      />
-    )
-  }
-}
+const SearchCepContainer = ({ address, handleSubmit }) => (
+  <SearchCep
+    {...address}
+    handleSubmit={handleSubmit}
+  />
+)
 
 const mapStateToProps = (state) => ({
   address: state.address
 })
 
-const mapDispatchToProps = { updateAddress }
+const mapDispatchToProps = (dispatch) => ({
+  handleSubmit: (e) => {
+    e.preventDefault()
+    dispatch(fetchAddress(e.target.cep.value))
+  }
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchCepContainer)
