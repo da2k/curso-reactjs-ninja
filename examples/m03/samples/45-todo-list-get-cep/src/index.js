ab42a2fc5af5a19db759d41a9f1afc4359f554f3
Fernando Daciuk <f.daciuk@gmail.com>
2017-12-12 15:08:13 -0200

    Todo List + CEP: Add configuration for configure store on Redux

--- examples/m03/samples/45-todo-list-get-cep/src/index.js
+++ examples/m03/samples/45-todo-list-get-cep/src/index.js
@@ -3,50 +3,11 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
-import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import App from './app'
-import reducer from 'reducers'
+import configureStore from './redux-flow/configure-store'
 
-const logger = ({ dispatch, getState }) => (next) => (action) => {
-  console.log('LOGGER::will dispatch:', action)
-  const nextAction = next(action)
-  console.log('LOGGER::next action:', nextAction)
-  return nextAction
-}
-
-const thunk = ({ dispatch, getState }) => (next) => (action) => {
-  if (typeof action === 'function') {
-    return action(dispatch, getState)
-  }
-  return next(action)
-}
-
-const store = createStore(reducer, applyMiddleware(logger, thunk))
-
-// -------------------
-store.dispatch(lazyAction())
-function lazyAction () {
-  return (dispatch, getState) => {
-    setTimeout(() => {
-      dispatch({
-        type: 'todos:ADD_TODO',
-        payload: {
-          text: 'Lazy Action',
-          id: '123'
-        }
-      })
-    }, 2000)
-  }
-}
-// -------------------
-
-const renderState = () => {
-  console.log('state:', store.getState())
-}
-
-store.subscribe(renderState)
-renderState()
+const store = configureStore()
 
 const renderApp = (NextApp) => {
   render(
@@ -65,10 +26,5 @@ if (module.hot) {
   module.hot.accept('./app', () => {
     const NextApp = require('./app').default
     renderApp(NextApp)
-  })
-
-  module.hot.accept('reducers', () => {
-    const nextReducer = require('reducers').default
-    store.replaceReducer(nextReducer)
   })
 }
