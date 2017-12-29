4e989a924b40a3cd82a3ae05d6ebd7b3aff1da11
Fernando Daciuk <f.daciuk@gmail.com>
2017-12-29 16:30:36 -0200

    Todo List + CEP: Usage for two or more store enhancers

--- examples/m03/samples/48-todo-list-get-cep/src/redux-flow/configure-store/index.js
+++ examples/m03/samples/48-todo-list-get-cep/src/redux-flow/configure-store/index.js
@@ -1,11 +1,12 @@
 'use strict'
 
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from 'reducers'
 
 export default ({ initialState } = {}) => {
-  const store = createStore(rootReducer, initialState, applyMiddleware(logger, thunk))
+  const enhancer = compose(applyMiddleware(thunk), logger())
+  const store = createStore(rootReducer, initialState, enhancer)
 
   if (module.hot) {
     module.hot.accept('reducers', () => {
@@ -17,12 +18,6 @@ export default ({ initialState } = {}) => {
   return store
 }
 
-const logger = ({ dispatch, getState }) => (next) => (action) => {
-  console.group(`LOGGER->${action.type}`)
-  console.log('will dispatch:', action)
-  console.log('state:', getState())
-  const nextAction = next(action)
-  console.log('next state:', getState())
-  console.groupEnd(`LOGGER->${action.type}`)
-  return nextAction
-}
+const logger = () => window.__REDUX_DEVTOOLS_EXTENSION__
+  ? window.__REDUX_DEVTOOLS_EXTENSION__()
+  : (x) => x
