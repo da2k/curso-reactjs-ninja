bc106f9b0439919d6a7c35e59e61ef77ddb8aadc
Fernando Daciuk <f.daciuk@gmail.com>
2018-01-28 18:38:51 -0200

    Reactflix: Add 16

---
+++ examples/m03/samples/49-reactflix/16/src/components/videos-list.js
@@ -0,0 +1,74 @@
+'use strict'
+
+import React from 'react'
+import { connect } from 'react-redux'
+import styled from 'styled-components'
+import Play from 'components/play'
+
+const VideosList = ({ videos, handleClick }) => (
+  <Container>
+    {Object.keys(videos).map((id) => (
+      <Video key={id}>
+        <VideoLink href='#' onClick={handleClick(id)}>
+          <VideoThumb>
+            <PlayStyled />
+          </VideoThumb>
+          <VideoTitle>{videos[id].title}</VideoTitle>
+        </VideoLink>
+      </Video>
+    ))}
+  </Container>
+)
+
+const PlayStyled = styled(Play)`
+  fill: #999;
+  height: 50px;
+  transition: all .15s ease-in-out;
+  width: 50px;
+`
+
+const Video = styled.section`
+  &:hover ${PlayStyled} {
+    transform: scale(1.5);
+  }
+`
+
+const VideoLink = styled.a`
+  color: inherit;
+`
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 10px;
+
+  & ${Video} {
+    flex: 1 1 300px;
+    margin: 0 5px 5px;
+  }
+`
+
+const VideoThumb = styled.div`
+  align-items: center;
+  border: 1px solid #999;
+  display: flex;
+  height: 150px;
+  justify-content: center;
+`
+
+const VideoTitle = styled.h2`
+  font-size: 18px;
+`
+
+const mapStateToProps = (state) => ({
+  videos: state.videos
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  handleClick: (id) => (e) => {
+    e.preventDefault()
+    console.log('clicou!', id)
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(VideosList)
