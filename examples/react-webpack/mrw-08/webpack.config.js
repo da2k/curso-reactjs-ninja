ba98e557f34678f13e5145fd2c925ea16ee249c6
Fernando Daciuk <f.daciuk@gmail.com>
2018-03-15 18:22:50 -0300

    Add MRW08 code

--- examples/react-webpack/mrw-08/webpack.config.js
+++ examples/react-webpack/mrw-08/webpack.config.js
@@ -1,17 +1,27 @@
 'use strict'
 
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   devtool: 'source-map',
 
-  entry: path.join(__dirname, 'src', 'index'),
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, 'src', 'index')
+  ],
 
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
 
   module: {
     loaders: [{
