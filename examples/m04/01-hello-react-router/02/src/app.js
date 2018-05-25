3cfc71d40db31f224f178da49910479f84177393
Fernando Daciuk <f.daciuk@gmail.com>
2018-05-25 14:02:41 -0300

    Hello RR4: exact prop

--- examples/m04/01-hello-react-router/02/src/app.js
+++ examples/m04/01-hello-react-router/02/src/app.js
@@ -6,39 +6,19 @@ import { BrowserRouter, Link, Route } from 'react-router-dom'
 import './css/style.css'
 
 class App extends PureComponent {
-  constructor () {
-    super()
-    this.state = {
-      title: '...',
-      Component: 'div'
-    }
-  }
-
-  getTitle () {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve('My app with async / await!')
-      }, 2000)
-    })
-  }
-
-  async componentDidMount () {
-    const title = await import('components/title')
-
-    this.setState({
-      title: await this.getTitle(),
-      Component: title.default
-    })
-  }
-
   render () {
     return (
       <BrowserRouter>
         <div>
-          <this.state.Component>{this.state.title}</this.state.Component>
-
+          <Route path='/' exact component={Home} />
           <Route path='/sobre' component={Sobre} />
-          <Link to='/sobre'>Sobre</Link>
+          <Route path='/contato' component={Contato} />
+
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/sobre'>Sobre</Link></li>
+            <li><Link to='/contato'>Contato</Link></li>
+          </ul>
 
         </div>
       </BrowserRouter>
@@ -46,8 +26,16 @@ class App extends PureComponent {
   }
 }
 
+const Home = () => (
+  <h1>Home</h1>
+)
+
 const Sobre = () => (
   <h1>Sobre</h1>
+)
+
+const Contato = () => (
+  <h1>Contato</h1>
 )
 
 export default App
