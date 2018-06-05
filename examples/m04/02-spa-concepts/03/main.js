15c5cf27ca9bf6f86d214f54fa4cd0a2fa8a2bcd
Fernando Daciuk <f.daciuk@gmail.com>
2018-06-05 18:32:52 -0300

    M04 - Spa concepts - History API (onpopstate)

--- examples/m04/02-spa-concepts/03/main.js
+++ examples/m04/02-spa-concepts/03/main.js
@@ -8,12 +8,28 @@
     $link.addEventListener('click', handleClick, false)
   })
 
+  window.onpopstate = function (e) {
+    console.log('state:', e.state)
+    navigation({ url: e.state.url })
+  }
+
+  function navigation (state) {
+    var titles = {
+      '/': 'Home',
+      '/sobre': 'Sobre'
+    }
+
+    $h1.textContent = titles[state.url]
+  }
+
   function handleClick (e) {
     e.preventDefault()
-    var state = {}
     var title = ''
     var url = e.target.getAttribute('href')
+    var state = {
+      url: url
+    }
     history.pushState(state, title, url)
-    $h1.textContent = url
+    navigation({ url: url })
   }
 })()
