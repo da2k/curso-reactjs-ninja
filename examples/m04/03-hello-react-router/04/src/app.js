3c9dbcc681c8f3f15864915d8b7215bd9e64e025
Fernando Daciuk <f.daciuk@gmail.com>
2018-07-03 15:06:01 -0300

    Hello RR4: Fix Link - add exact

--- examples/m04/03-hello-react-router/04/src/app.js
+++ examples/m04/03-hello-react-router/04/src/app.js
@@ -15,7 +15,7 @@ class App extends PureComponent {
       <BrowserRouter>
         <div>
           <ul>
-            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/' exact>Home</Link></li>
             <li><Link to='/sobre'>Sobre</Link></li>
             <li><Link to='/contato'>Contato</Link></li>
             <li><Link to='/blog'>Blog</Link></li>
