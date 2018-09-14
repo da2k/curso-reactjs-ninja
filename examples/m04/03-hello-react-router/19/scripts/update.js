1cae284e5aa418bc783b6a7409adebaae03d1ab4
Fernando Daciuk <f.daciuk@gmail.com>
2018-09-14 15:49:27 -0300

    Hello RR4: Add 03/19

---
+++ examples/m04/03-hello-react-router/19/scripts/update.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const { spawn } = require('child_process')
+const pkg = require('../package.json')
+
+const dependencies = Object.keys(pkg.dependencies)
+const devDependencies = Object.keys(pkg.devDependencies)
+
+const add = (args) => {
+  return spawn('yarn', ['add'].concat(args), { stdio: 'inherit' })
+}
+
+const addDev = (args) => {
+  return add(['--dev'].concat(args))
+}
+
+add(dependencies).on('close', () => {
+  addDev(devDependencies.concat('react-hot-loader@next'))
+    .on('close', (code) => process.exit(code))
+})
