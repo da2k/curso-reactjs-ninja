54105d91960212d7a3713e5e7013c664886b5849
Fernando Daciuk <f.daciuk@gmail.com>
2019-01-16 15:39:11 -0200

    Reactzzaria: componentDidCatch

--- examples/m04/04-reactzzaria/03/sistema-pedidos/src/error.js
+++ examples/m04/04-reactzzaria/03/sistema-pedidos/src/error.js
@@ -3,8 +3,14 @@ import { PureComponent } from 'react'
 class ErrorBoundary extends PureComponent {
   state = { hasError: false }
 
-  static getDerivedStateFromError () {
+  static getDerivedStateFromError (error) {
+    console.log('error getDerivedStateFromError:', error.message)
     return { hasError: true }
+  }
+
+  componentDidCatch (error, info) {
+    console.log('error:', error)
+    console.log('info:', info.componentStack)
   }
 
   render () {
