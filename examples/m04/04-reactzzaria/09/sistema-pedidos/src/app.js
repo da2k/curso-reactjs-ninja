9af74a5f0c9c914cb9e28ed7d5575e4dc67009b6
Fernando Daciuk <f.daciuk@gmail.com>
2019-02-18 17:23:00 -0300

    Reactzzaria: Update dependencies and add Main and Login pages

--- examples/m04/04-reactzzaria/09/sistema-pedidos/src/app.js
+++ examples/m04/04-reactzzaria/09/sistema-pedidos/src/app.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import { MainPage } from './pages/main'
+import { Login } from './pages/login'
 
 const App = () => (
-  <h1>App</h1>
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' component={MainPage} exact />
+      <Route path='/login' component={Login} />
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App
---
+++ examples/m04/04-reactzzaria/09/sistema-pedidos/src/pages/login/login.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Login = () => (
+  <h1>Login</h1>
+)
+
+export default Login
