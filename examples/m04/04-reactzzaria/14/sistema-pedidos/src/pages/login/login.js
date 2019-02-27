84288500a504ca9ae6cdefc2369b4573165b4e42
Fernando Daciuk <f.daciuk@gmail.com>
2019-02-27 17:22:22 -0300

    Reactzzaria: Add 14

---
+++ examples/m04/04-reactzzaria/14/sistema-pedidos/src/pages/login/login.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import { Button } from '@material-ui/core'
+import { ReactComponent as Logo } from './logo-react-zzaria.svg'
+
+const Login = () => (
+  <Fragment>
+    <Logo />
+    <Button>Entrar com GitHub</Button>
+  </Fragment>
+)
+
+export default Login
