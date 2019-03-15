4b0262a15cc8e91e8c762944885da757b1fb53b2
Fernando Daciuk <f.daciuk@gmail.com>
2019-03-15 11:25:41 -0300

    Hooks: Add 02

---
+++ examples/m04/05-hooks/02/src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+
+const App = () => (
+  <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+    <CounterClass />
+    <CounterFunction />
+  </div>
+)
+
+class CounterClass extends React.PureComponent {
+  state = {
+    counter: 0
+  }
+
+  render () {
+    return (
+      <Counter
+        counter={this.state.counter}
+        increment={() => {
+          this.setState({
+            counter: this.state.counter + 1
+          })
+        }}
+
+        decrement={() => {
+          this.setState({
+            counter: this.state.counter - 1
+          })
+        }}
+      />
+    )
+  }
+}
+
+function CounterFunction () {
+  const [counter, setCounter] = useState(0)
+
+  return (
+    <Counter
+      counter={counter}
+      increment={() => {
+        setCounter(counter + 1)
+      }}
+
+      decrement={() => {
+        setCounter(counter - 1)
+      }}
+    />
+  )
+}
+
+const Counter = ({ counter, increment, decrement }) => (
+  <div style={{ textAlign: 'center' }}>
+    <h1>{counter}</h1>
+    <button onClick={decrement}>-</button>
+    <button onClick={increment}>+</button>
+  </div>
+)
+
+export default App
