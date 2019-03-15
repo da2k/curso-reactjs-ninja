b5b3ae27fd1fd92599bd678afaa0ea1fd6d81df0
Fernando Daciuk <f.daciuk@gmail.com>
2019-03-15 17:08:06 -0300

    Hooks: useEffect: conditional update

--- examples/m04/05-hooks/06/src/App.js
+++ examples/m04/05-hooks/06/src/App.js
@@ -9,15 +9,19 @@ const App = () => (
 
 class CounterClass extends React.PureComponent {
   state = {
-    counter: 0
+    counter: 0,
+    toggle: false
   }
 
   componentDidMount () {
     this.updateDocumentTitle()
   }
 
-  componentDidUpdate () {
-    this.updateDocumentTitle()
+  componentDidUpdate (prevProps, prevState) {
+    if (prevState.counter !== this.state.counter) {
+      console.log('componentDidUpdate')
+      this.updateDocumentTitle()
+    }
   }
 
   updateDocumentTitle () {
@@ -26,42 +30,55 @@ class CounterClass extends React.PureComponent {
 
   render () {
     return (
-      <Counter
-        counter={this.state.counter}
-        increment={() => {
-          this.setState((prevState) => ({
-            counter: prevState.counter + 1
-          }))
-        }}
+      <>
+        <Counter
+          counter={this.state.counter}
+          increment={() => {
+            this.setState((prevState) => ({
+              counter: prevState.counter + 1
+            }))
+          }}
 
-        decrement={() => {
-          this.setState((prevState) => ({
-            counter: prevState.counter - 1
-          }))
-        }}
-      />
+          decrement={() => {
+            this.setState((prevState) => ({
+              counter: prevState.counter - 1
+            }))
+          }}
+        />
+        {this.state.toggle && <h1>Toggle</h1>}
+        <button onClick={() => this.setState({
+          toggle: !this.state.toggle
+        })}>Toggle</button>
+      </>
     )
   }
 }
 
 function CounterFunction () {
   const [counter, setCounter] = useState(0)
+  const [toggle, setToggle] = useState(false)
 
   useEffect(() => {
+    console.log('useEffect')
     document.title = `CounterFunction: ${counter}`
-  })
+  }, [counter])
 
   return (
-    <Counter
-      counter={counter}
-      increment={() => {
-        setCounter(counter => counter + 1)
-      }}
+    <>
+      <Counter
+        counter={counter}
+        increment={() => {
+          setCounter(counter => counter + 1)
+        }}
 
-      decrement={() => {
-        setCounter(counter => counter - 1)
-      }}
-    />
+        decrement={() => {
+          setCounter(counter => counter - 1)
+        }}
+      />
+
+      {toggle && <h1>Toggle</h1>}
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+    </>
   )
 }
 
