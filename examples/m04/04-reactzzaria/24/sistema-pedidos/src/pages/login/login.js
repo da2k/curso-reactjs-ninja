da57672423d00cb3e3b62011c945c51d3d0b434b
Fernando Daciuk <f.daciuk@gmail.com>
2019-03-19 16:32:45 -0300

    Reactzzaria: Add 24

---
+++ examples/m04/04-reactzzaria/24/sistema-pedidos/src/pages/login/login.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import { Button, Grid } from '@material-ui/core'
+import { ReactComponent as MainLogo } from './logo-react-zzaria.svg'
+
+const config = {
+  apiKey: 'AIzaSyADubq8Pf-_nC6cM52PGZJLAJ_yT4UkWto',
+  authDomain: 'reactzzaria-12649.firebaseapp.com',
+  databaseURL: 'https://reactzzaria-12649.firebaseio.com',
+  projectId: 'reactzzaria-12649',
+  storageBucket: 'reactzzaria-12649.appspot.com',
+  messagingSenderId: '190402590347'
+}
+
+firebase.initializeApp(config)
+
+const login = () => {
+  const provider = new firebase.auth.GithubAuthProvider()
+  firebase.auth().signInWithRedirect(provider)
+}
+
+function Login () {
+  const [userInfo, setUserInfo] = useState({
+    isUserLoggedIn: false,
+    user: null
+  })
+
+  const { isUserLoggedIn, user } = userInfo
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log('dados do usuário:', user)
+      setUserInfo({
+        isUserLoggedIn: !!user,
+        user
+      })
+    })
+  }, [])
+
+  const logout = () => {
+    firebase.auth().signOut().then(() => {
+      console.log('deslogou!')
+      setUserInfo({
+        isUserLoggedIn: false,
+        user: null
+      })
+    })
+  }
+
+  return (
+    <Container>
+      <Grid container justify='center' spacing={40}>
+        <Grid item>
+          <Logo />
+        </Grid>
+
+        <Grid item xs={12} container justify='center'>
+          {isUserLoggedIn && (
+            <>
+              <pre>{user.displayName}</pre>
+              <Button variant='contained' onClick={logout}>
+                Sair
+              </Button>
+            </>
+          )}
+
+          {!isUserLoggedIn && (
+            <GitHubButton onClick={login}>
+              Entrar com GitHub
+            </GitHubButton>
+          )}
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  padding: 20px;
+`
+
+const Logo = styled(MainLogo)`
+  width: 100%;
+`
+
+const GitHubButton = styled(Button).attrs({
+  variant: 'contained',
+  fullWidth: true
+})`
+  && {
+    font-size: 25px;
+    max-width: 480px;
+    padding: 15px;
+    text-transform: none;
+  }
+`
+
+export default Login
