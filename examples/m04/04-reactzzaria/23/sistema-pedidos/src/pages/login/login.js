84727387ac91b7bb3223a9062f57a414bd3d0e2e
Fernando Daciuk <f.daciuk@gmail.com>
2019-03-19 16:28:49 -0300

    Reactzzaria: Update class component to function with hooks

--- examples/m04/04-reactzzaria/23/sistema-pedidos/src/pages/login/login.js
+++ examples/m04/04-reactzzaria/23/sistema-pedidos/src/pages/login/login.js
@@ -1,11 +1,11 @@
-import React, { PureComponent } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import { Button, Grid } from '@material-ui/core'
 import { ReactComponent as MainLogo } from './logo-react-zzaria.svg'
 
-var config = {
+const config = {
   apiKey: 'AIzaSyADubq8Pf-_nC6cM52PGZJLAJ_yT4UkWto',
   authDomain: 'reactzzaria-12649.firebaseapp.com',
   databaseURL: 'https://reactzzaria-12649.firebaseio.com',
@@ -16,67 +16,65 @@ var config = {
 
 firebase.initializeApp(config)
 
-class Login extends PureComponent {
-  state = {
+const login = () => {
+  const provider = new firebase.auth.GithubAuthProvider()
+  firebase.auth().signInWithRedirect(provider)
+}
+
+function Login () {
+  const [userInfo, setUserInfo] = useState({
     isUserLoggedIn: false,
     user: null
-  }
+  })
 
-  componentDidMount () {
+  const { isUserLoggedIn, user } = userInfo
+
+  useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       console.log('dados do usuário:', user)
-      this.setState({
+      setUserInfo({
         isUserLoggedIn: !!user,
         user
       })
     })
-  }
+  }, [])
 
-  login () {
-    const provider = new firebase.auth.GithubAuthProvider()
-    firebase.auth().signInWithRedirect(provider)
-  }
-
-  logout = () => {
+  const logout = () => {
     firebase.auth().signOut().then(() => {
       console.log('deslogou!')
-      this.setState({
+      setUserInfo({
         isUserLoggedIn: false,
         user: null
       })
     })
   }
 
-  render () {
-    const { isUserLoggedIn, user } = this.state
-
-    return (
-      <Container>
-        <Grid container justify='center' spacing={40}>
-          <Grid item>
-            <Logo />
-          </Grid>
-
-          <Grid item xs={12} container justify='center'>
-            {isUserLoggedIn && (
-              <>
-                <pre>{user.displayName}</pre>
-                <Button variant='contained' onClick={this.logout}>
-                  Sair
-                </Button>
-              </>
-            )}
-
-            {!isUserLoggedIn && (
-              <GitHubButton onClick={this.login}>
-                Entrar com GitHub
-              </GitHubButton>
-            )}
-          </Grid>
+  return (
+    <Container>
+      <Grid container justify='center' spacing={40}>
+        <Grid item>
+          <Logo />
         </Grid>
-      </Container>
-    )
-  }
+
+        <Grid item xs={12} container justify='center'>
+          {isUserLoggedIn && (
+            <>
+              <pre>{user.displayName}</pre>
+              <Button variant='contained' onClick={logout}>
+                Sair
+              </Button>
+            </>
+          )}
+
+          {!isUserLoggedIn && (
+            <GitHubButton onClick={login}>
+              Entrar com GitHub
+            </GitHubButton>
+          )}
+        </Grid>
+      </Grid>
+    </Container>
+  )
 }
 
 const Container = styled.div`
