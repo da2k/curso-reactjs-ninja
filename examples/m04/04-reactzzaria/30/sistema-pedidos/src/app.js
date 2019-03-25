57b80f97b118441f2c645ad43baa0096db5369fd
Fernando Daciuk <f.daciuk@gmail.com>
2019-03-25 15:23:59 -0300

    Reactzzaria: Refactory on components that depends on authentication

--- examples/m04/04-reactzzaria/30/sistema-pedidos/src/app.js
+++ examples/m04/04-reactzzaria/30/sistema-pedidos/src/app.js
@@ -1,11 +1,25 @@
-import React, { lazy, Suspense } from 'react'
+import React, { lazy, useEffect, useContext, Suspense } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { LinearProgress } from '@material-ui/core'
+import firebase from 'services/firebase'
+import { AuthContext } from 'contexts/auth'
 
 const MainPage = lazy(() => import('pages/main'))
 const Login = lazy(() => import('pages/login'))
 
 function App () {
+  const { setUserInfo } = useContext(AuthContext)
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log('dados do usuário:', user)
+      setUserInfo({
+        isUserLoggedIn: !!user,
+        user
+      })
+    })
+  }, [])
+
   return (
     <Suspense fallback={<LinearProgress />}>
       <Switch>
--- examples/m04/04-reactzzaria/30/sistema-pedidos/src/contexts/auth.js
+++ examples/m04/04-reactzzaria/30/sistema-pedidos/src/contexts/auth.js
@@ -1,18 +1,36 @@
-import React, { createContext, useCallback } from 'react'
+import React, { createContext, useCallback, useState } from 'react'
 import t from 'prop-types'
 import firebase from 'services/firebase'
 
 export const AuthContext = createContext()
 
 function Auth ({ children }) {
+  const [userInfo, setUserInfo] = useState({
+    isUserLoggedIn: false,
+    user: null
+  })
+
   const login = useCallback(() => {
     const provider = new firebase.auth.GithubAuthProvider()
     firebase.auth().signInWithRedirect(provider)
   }, [])
 
+  const logout = useCallback(() => {
+    firebase.auth().signOut().then(() => {
+      console.log('deslogou!')
+      setUserInfo({
+        isUserLoggedIn: false,
+        user: null
+      })
+    })
+  }, [])
+
   return (
     <AuthContext.Provider value={{
-      login
+      login,
+      logout,
+      userInfo,
+      setUserInfo
     }}>
       {children}
     </AuthContext.Provider>
