a6958a0048a43e9c94927ec9ed59741dadf80567
Fernando Daciuk <f.daciuk@gmail.com>
2019-03-26 15:59:42 -0300

    Reactzzaria: Add redirect rules based on user status (logged in or logged out)

--- examples/m04/04-reactzzaria/31/sistema-pedidos/src/app.js
+++ examples/m04/04-reactzzaria/31/sistema-pedidos/src/app.js
@@ -1,5 +1,6 @@
 import React, { lazy, useEffect, useContext, Suspense } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import t from 'prop-types'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import { LinearProgress } from '@material-ui/core'
 import firebase from 'services/firebase'
 import { AuthContext } from 'contexts/auth'
@@ -7,8 +8,10 @@ import { AuthContext } from 'contexts/auth'
 const MainPage = lazy(() => import('pages/main'))
 const Login = lazy(() => import('pages/login'))
 
-function App () {
-  const { setUserInfo } = useContext(AuthContext)
+function App ({ location }) {
+  const { userInfo, setUserInfo } = useContext(AuthContext)
+
+  const { isUserLoggedIn } = userInfo
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
@@ -20,6 +23,30 @@ function App () {
     })
   }, [])
 
+  if (isUserLoggedIn) {
+    console.log('usuário está logado')
+    if (location.pathname === '/login') {
+      console.log(
+        `usuário está logado E está na página de login.
+        Redirecionar para home /`
+      )
+      return <Redirect to='/' />
+    } else {
+      console.log('usuário está logado MAS NÃO está na página de login.')
+    }
+  } else {
+    console.log('usuário não está logado')
+    if (location.pathname !== '/login') {
+      console.log(`
+        usuário não está logado, NEM está na página de login.
+        Redirecionar para /login
+      `)
+      return <Redirect to='/login' />
+    } else {
+      console.log('usuário não está logado E está na página de login.')
+    }
+  }
+
   return (
     <Suspense fallback={<LinearProgress />}>
       <Switch>
@@ -28,6 +55,10 @@ function App () {
       </Switch>
     </Suspense>
   )
+}
+
+App.propTypes = {
+  location: t.object.isRequired
 }
 
 export default App
