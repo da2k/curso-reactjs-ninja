8a61fc2cf9a858a07a06c12dfca5be879c43b19d
Fernando Daciuk <f.daciuk@gmail.com>
2019-03-26 16:24:36 -0300

    Reactzzaria: Add state to check once if user in logged in ou out

--- examples/m04/04-reactzzaria/32/sistema-pedidos/src/app.js
+++ examples/m04/04-reactzzaria/32/sistema-pedidos/src/app.js
@@ -1,4 +1,4 @@
-import React, { lazy, useEffect, useContext, Suspense } from 'react'
+import React, { lazy, useEffect, useContext, useState, Suspense } from 'react'
 import t from 'prop-types'
 import { Redirect, Route, Switch } from 'react-router-dom'
 import { LinearProgress } from '@material-ui/core'
@@ -9,7 +9,8 @@ const MainPage = lazy(() => import('pages/main'))
 const Login = lazy(() => import('pages/login'))
 
 function App ({ location }) {
-  const { userInfo, setUserInfo } = useContext(AuthContext)
+  const { userInfo, setUserInfo, logout } = useContext(AuthContext)
+  const [didCheckUserIn, setDidCheckUserIn] = useState(false)
 
   const { isUserLoggedIn } = userInfo
 
@@ -20,8 +21,18 @@ function App ({ location }) {
         isUserLoggedIn: !!user,
         user
       })
+      setDidCheckUserIn(true)
     })
+
+    window.logout = logout
   }, [])
+
+  if (!didCheckUserIn) {
+    console.log('ainda não checou se usuário está logado ou não.')
+    return <LinearProgress />
+  }
+
+  console.log('já checou se usuário está logado ou não')
 
   if (isUserLoggedIn) {
     console.log('usuário está logado')
