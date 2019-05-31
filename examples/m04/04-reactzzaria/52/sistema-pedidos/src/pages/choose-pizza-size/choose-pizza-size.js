3b06d0a9c40160d8f029c9a6998d98cc80a91120
Fernando Daciuk <f.daciuk@gmail.com>
2019-05-31 17:11:09 -0300

    Reactzzaria: Add link to Choose Pizza's Flavours page

--- examples/m04/04-reactzzaria/52/sistema-pedidos/src/pages/choose-pizza-size/choose-pizza-size.js
+++ examples/m04/04-reactzzaria/52/sistema-pedidos/src/pages/choose-pizza-size/choose-pizza-size.js
@@ -1,9 +1,11 @@
 import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import {
+  Card,
+  CardActionArea as MaterialCardActionArea,
   Divider as MaterialDivider,
   Grid,
-  Paper,
   Typography
 } from '@material-ui/core'
 import { AuthContext } from 'contexts/auth'
@@ -27,20 +29,22 @@ const ChoosePizzaSize = () => {
       <PizzasGrid>
         {pizzaSizes.map((pizza) => (
           <Grid item key={pizza.id} xs>
-            <PaperPizza>
-              <Pizza>
-                <PizzaText>{pizza.size}cm</PizzaText>
-              </Pizza>
+            <Card>
+              <CardActionArea to='/sabores-da-pizza'>
+                <Pizza>
+                  <PizzaText>{pizza.size}cm</PizzaText>
+                </Pizza>
 
-              <Divider />
+                <Divider />
 
-              <Typography variant='h5'>{pizza.name}</Typography>
-              <Typography>
-                {pizza.slices} fatias, {' '}
-                {pizza.flavours} {' '}
-                {singularOrPlural(pizza.flavours, 'sabor', 'sabores')}
-              </Typography>
-            </PaperPizza>
+                <Typography variant='h5'>{pizza.name}</Typography>
+                <Typography>
+                  {pizza.slices} fatias, {' '}
+                  {pizza.flavours} {' '}
+                  {singularOrPlural(pizza.flavours, 'sabor', 'sabores')}
+                </Typography>
+              </CardActionArea>
+            </Card>
           </Grid>
         ))}
       </PizzasGrid>
@@ -69,7 +73,9 @@ const PizzasGrid = styled(Grid).attrs({
   padding: 20px;
 `
 
-const PaperPizza = styled(Paper)`
+const CardActionArea = styled(MaterialCardActionArea).attrs({
+  component: Link
+})`
   align-items: center;
   display: flex;
   flex-direction: column;
@@ -79,6 +85,7 @@ const PaperPizza = styled(Paper)`
 
 const Pizza = styled.div`
   align-items: center;
+  background: #fff;
   border: 1px solid #ccc;
   border-radius: 50%;
   display: flex;
@@ -86,6 +93,7 @@ const Pizza = styled.div`
   justify-content: center;
   position: relative;
   width: 200px;
+  z-index: 1;
 
   &::before,
   &::after {
