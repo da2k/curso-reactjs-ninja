675304edddf461eb4f964aff9efbbe01f1702b64
Fernando Daciuk <f.daciuk@gmail.com>
2019-07-08 15:46:50 -0300

    Reactzzaria: Style for selected flavours

--- examples/m04/04-reactzzaria/61/sistema-pedidos/src/pages/choose-pizza-flavours/choose-pizza-flavours.js
+++ examples/m04/04-reactzzaria/61/sistema-pedidos/src/pages/choose-pizza-flavours/choose-pizza-flavours.js
@@ -3,7 +3,7 @@ import t from 'prop-types'
 import styled from 'styled-components'
 import { Redirect } from 'react-router-dom'
 import {
-  Card,
+  Card as MaterialCard,
   Grid,
   Typography
 } from '@material-ui/core'
@@ -57,10 +57,9 @@ const ChoosePizzaFlavours = ({ location }) => {
       <PizzasGrid>
         {pizzasFlavours.map((pizza) => (
           <Grid item key={pizza.id} xs>
-            <Card>
+            <Card checked={!!checkboxes[pizza.id]}>
               <Label>
-                <input
-                  type='checkbox'
+                <Checkbox
                   checked={!!checkboxes[pizza.id]}
                   onChange={handleChangeCheckbox(pizza.id)}
                 />
@@ -90,9 +89,20 @@ function checkboxesChecked (checkboxes) {
   return Object.values(checkboxes).filter(Boolean)
 }
 
+const Card = styled(MaterialCard)`
+  border: 2px solid transparent;
+  border-color: ${({ checked }) => checked ? '#000' : ''};
+`
+
 const Label = styled(CardLink).attrs({
   component: 'label'
 })``
+
+const Checkbox = styled.input.attrs({
+  type: 'checkbox'
+})`
+  display: none;
+`
 
 const Img = styled.img`
   width: 200px;
