ad89ee280de66d6df96759941a6e661e372d38a9
Fernando Daciuk <f.daciuk@gmail.com>
2019-07-19 18:24:05 -0300

    Reactzzaria: Add 65

---
+++ examples/m04/04-reactzzaria/65/sistema-pedidos/src/ui/divider.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { Divider as MaterialDivider } from '@material-ui/core'
+
+const Divider = styled(MaterialDivider)`
+  margin: ${({ theme }) => theme.spacing(3, 0)};
+  width: 100%;
+`
+
+export default Divider
