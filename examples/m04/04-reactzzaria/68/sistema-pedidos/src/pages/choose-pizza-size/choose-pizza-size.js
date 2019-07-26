e379e63128389224a58d0038c67508f38f50be64
Fernando Daciuk <f.daciuk@gmail.com>
2019-07-26 15:15:28 -0300

    Reactzzaria: Add 68

---
+++ examples/m04/04-reactzzaria/68/sistema-pedidos/src/pages/choose-pizza-size/choose-pizza-size.js
@@ -0,0 +1,113 @@
+import React, { useContext } from 'react'
+import styled from 'styled-components'
+import {
+  Card,
+  Grid,
+  Typography
+} from '@material-ui/core'
+import {
+  CardLink,
+  Content,
+  Divider,
+  H3,
+  H4,
+  PizzasGrid
+} from 'ui'
+import { singularOrPlural } from 'utils'
+import { AuthContext } from 'contexts/auth'
+import pizzaSizes from 'fake-data/pizzas-sizes'
+
+import { CHOOSE_PIZZA_FLAVOURS } from 'routes'
+
+const ChoosePizzaSize = () => {
+  const { userInfo } = useContext(AuthContext)
+
+  return (
+    <Content>
+      <Grid container direction='column' alignItems='center'>
+        <H3>
+          O que vai ser hoje, {userInfo.user.firstName}? =)
+        </H3>
+
+        <H4>
+          Escolha o tamanho da pizza:
+        </H4>
+      </Grid>
+
+      <PizzasGrid>
+        {pizzaSizes.map((pizza) => (
+          <Grid item key={pizza.id} xs>
+            <Card>
+              <CardLink to={{
+                pathname: CHOOSE_PIZZA_FLAVOURS,
+                state: pizza
+              }}>
+                <Pizza>
+                  <PizzaText>{pizza.size}cm</PizzaText>
+                </Pizza>
+
+                <Divider />
+
+                <Typography variant='h5'>{pizza.name}</Typography>
+                <Typography>
+                  {pizza.slices} fatias, {' '}
+                  {pizza.flavours} {' '}
+                  {singularOrPlural(pizza.flavours, 'sabor', 'sabores')}
+                </Typography>
+              </CardLink>
+            </Card>
+          </Grid>
+        ))}
+      </PizzasGrid>
+    </Content>
+  )
+}
+
+const Pizza = styled.div`
+  align-items: center;
+  background: ${({ theme }) => theme.palette.common.white};
+  border: 1px solid ${({ theme }) => theme.palette.grey.A100};
+  border-radius: 50%;
+  display: flex;
+  height: 200px;
+  justify-content: center;
+  position: relative;
+  width: 200px;
+  z-index: 1;
+
+  &::before,
+  &::after {
+    background: ${({ theme }) => theme.palette.grey.A100};
+    content: '';
+    position: absolute;
+    transform: rotate(45deg);
+  }
+
+  &::before {
+    height: 1px;
+    width: 160px;
+  }
+
+  &::after {
+    height: 160px;
+    width: 1px;
+  }
+`
+
+const PizzaText = styled(Typography).attrs({
+  variant: 'h5'
+})`
+  && {
+    align-items: center;
+    background: ${({ theme }) => theme.palette.common.white};
+    border-radius: 50%;
+    display: flex;
+    height: 80px;
+    justify-content: center;
+    position: relative;
+    width: 80px;
+    z-index: 1;
+  }
+`
+
+export default ChoosePizzaSize
---
+++ examples/m04/04-reactzzaria/68/sistema-pedidos/src/ui/index.js
@@ -0,0 +1,6 @@
+export { default as CardLink } from './card-link'
+export { default as Content } from './content'
+export { default as Divider } from './divider'
+export { default as HeaderContent } from './header-content'
+export { default as PizzasGrid } from './pizzas-grid'
+export { default as Title, H3, H4 } from './title'
