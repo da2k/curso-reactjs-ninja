1c57bb2bbd086bce5288099ecf97fba9c1772eb3
Fernando Daciuk <f.daciuk@gmail.com>
2019-07-26 14:48:23 -0300

    Reactzzaria: Add 66

---
+++ examples/m04/04-reactzzaria/66/sistema-pedidos/src/ui/content.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+const Content = styled.main`
+  padding: ${({ theme }) => theme.spacing(3)}px;
+`
+
+export default Content
---
+++ examples/m04/04-reactzzaria/66/sistema-pedidos/src/ui/pizzas-grid.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import { Grid } from '@material-ui/core'
+
+const PizzasGrid = styled(Grid).attrs({
+  container: true,
+  spacing: 2
+})`
+  padding: ${({ theme }) => theme.spacing(3)}px;
+`
+
+export default PizzasGrid
