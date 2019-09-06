66a52037c56133c3f8eec6b19ddddf4bd0223332
Fernando Daciuk <f.daciuk@gmail.com>
2019-09-06 17:23:36 -0300

    Reactzzaria: Add 88

---
+++ examples/m04/04-reactzzaria/88/sistema-pedidos/src/pages/checkout/checkout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Content } from 'ui'
+
+function Checkout () {
+  return (
+    <Content>
+      <h1>Checkout</h1>
+    </Content>
+  )
+}
+
+export default Checkout
