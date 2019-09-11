dfb90cb754f5cc0c09a85d42f9b18828ffcaf3b6
Fernando Daciuk <f.daciuk@gmail.com>
2019-09-11 12:22:12 -0300

    Reactzzaria: Add 91

---
+++ examples/m04/04-reactzzaria/91/sistema-pedidos/src/fake-data/pizzas-flavours.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    id: '0',
+    image: '/fake-data/images/pizza-calabresa.png',
+    name: 'Calabresa',
+    value: {
+      '0': 20,
+      '1': 30,
+      '2': 40
+    }
+  },
+
+  {
+    id: '1',
+    image: '/fake-data/images/pizza-frango-catupiry.png',
+    name: 'Frango com Catupiry',
+    value: {
+      '0': 25,
+      '1': 35,
+      '2': 45
+    }
+  },
+
+  {
+    id: '2',
+    image: '/fake-data/images/pizza-prestigio.jpg',
+    name: 'Prestígio',
+    value: {
+      '0': 30,
+      '1': 40,
+      '2': 50
+    }
+  }
+]
