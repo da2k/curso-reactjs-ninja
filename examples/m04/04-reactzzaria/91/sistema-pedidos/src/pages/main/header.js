97a2b7c5e31e45e7e74f17dc44ecad8929aacfa7
Fernando Daciuk <f.daciuk@gmail.com>
2019-09-11 17:23:19 -0300

    Reactzzaria: Add custom header for checkout page

--- examples/m04/04-reactzzaria/91/sistema-pedidos/src/pages/main/header.js
+++ examples/m04/04-reactzzaria/91/sistema-pedidos/src/pages/main/header.js
@@ -1,86 +1,30 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import {
   AppBar,
-  IconButton,
-  Menu,
-  MenuItem,
-  Toolbar as MaterialToolbar,
-  Typography
+  Toolbar as MaterialToolbar
 } from '@material-ui/core'
-import { AccountCircle } from '@material-ui/icons'
-import { ReactComponent as MainLogo } from 'images/logo-react-zzaria.svg'
-import { useAuth } from 'hooks'
-import { HOME } from 'routes'
+import HeaderCommon from './header-common'
+import HeaderCheckout from './header-checkout'
+import { CHECKOUT } from 'routes'
 
-const Header = () => {
-  const [anchorElement, setAnchorElement] = useState(null)
-  const { userInfo, logout } = useAuth()
-
-  const handleOpenMenu = (e) => {
-    setAnchorElement(e.target)
-  }
-
-  const handleClose = () => {
-    setAnchorElement(null)
-  }
-
-  return (
-    <AppBar>
-      <Toolbar>
-        <LogoContainer>
-          <LinkLogo to={HOME}>
-            <Logo />
-          </LinkLogo>
-        </LogoContainer>
-
-        <Typography color='inherit'>
-          Olá {userInfo.user.firstName} =)
-        </Typography>
-
-        <IconButton color='inherit' onClick={handleOpenMenu}>
-          <AccountCircle />
-        </IconButton>
-
-        <Menu
-          open={Boolean(anchorElement)}
-          onClose={handleClose}
-          anchorEl={anchorElement}
-        >
-          <MenuItem onClick={logout}>Sair</MenuItem>
-        </Menu>
-      </Toolbar>
-    </AppBar>
-  )
-}
+const Header = () => (
+  <AppBar>
+    <Toolbar>
+      <Switch>
+        <Route path={CHECKOUT} component={HeaderCheckout} />
+        <Route component={HeaderCommon} />
+      </Switch>
+    </Toolbar>
+  </AppBar>
+)
 
 const Toolbar = styled(MaterialToolbar)`
   && {
     margin: 0 auto;
     max-width: ${({ theme }) => theme.breakpoints.values.lg}px;
     width: 100%;
-  }
-`
-
-const LogoContainer = styled.div`
-  flex-grow: 1;
-`
-
-const LinkLogo = styled(Link)`
-  display: inline-block;
-`
-
-const Logo = styled(MainLogo)`
-  height: 50px;
-  width: 200px;
-
-  & path {
-    fill: ${({ theme }) => theme.palette.common.white};
-  }
-
-  & line {
-    stroke: ${({ theme }) => theme.palette.common.white};
   }
 `
 
