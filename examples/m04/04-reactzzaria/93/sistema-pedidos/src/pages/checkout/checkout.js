c853821fc12d5b9e485adff311f3f03b4b8611af
Fernando Daciuk <f.daciuk@gmail.com>
2019-10-08 16:32:51 -0300

    Reactzzaria: Add 93

---
+++ examples/m04/04-reactzzaria/93/sistema-pedidos/src/pages/checkout/checkout.js
@@ -0,0 +1,123 @@
+import React from 'react'
+import t from 'prop-types'
+import styled from 'styled-components'
+import {
+  Grid,
+  List,
+  ListItem,
+  Paper,
+  TextField as MaterialTextField,
+  Typography
+} from '@material-ui/core'
+import {
+  Content,
+  Footer,
+  Title as UiTitle
+} from 'ui'
+import { useOrder } from 'hooks'
+import { singularOrPlural } from 'utils'
+
+function Checkout () {
+  const { order } = useOrder()
+  console.log('order:', order)
+
+  return (
+    <>
+      <Content>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={6}>
+            <Title>Qual o endereço para entrega?</Title>
+            <PaperContainer>
+              <Grid container spacing={2}>
+                <TextField label='CEP' xs={4} autoFocus />
+                <Grid item xs={8} />
+
+                <TextField label='Rua' xs={9} />
+                <TextField label='Número' xs={3} />
+                <TextField label='Complemento' xs={12} />
+                <TextField label='Cidade' xs={9} />
+                <TextField label='Estado' xs={3} />
+              </Grid>
+            </PaperContainer>
+
+            <Title>Qual o seu telefone?</Title>
+            <PaperContainer>
+              <TextField label='Telefone' xs={4} />
+            </PaperContainer>
+          </Grid>
+
+          <Grid container item xs={12} md={6} direction='column'>
+            <Title>Informações do seu pedido:</Title>
+            <PaperContainer>
+              <List>
+                {order.pizzas.map((pizza, index) => {
+                  const { pizzaFlavours, pizzaSize, quantity } = pizza
+                  const { name, slices, flavours } = pizzaSize
+
+                  return (
+                    <ListItem key={index}>
+                      <Typography>
+                        {quantity} {' '}
+                        {singularOrPlural(quantity, 'pizza', 'pizzas')} {' '}
+                        <b>{name.toUpperCase()}</b> {'- '}
+                        ({slices} {singularOrPlural(slices, 'fatia', 'fatias')}, {' '}
+                        {flavours} {singularOrPlural(flavours, 'sabor', 'sabores')})
+
+                        <br />
+
+                        {singularOrPlural(pizzaFlavours.length, 'no sabor', 'nos sabores')}{' '}
+                        <b>{pizzaFlavours.map(({ name }) => name).join(', ')}</b>
+                      </Typography>
+                    </ListItem>
+                  )
+                })}
+              </List>
+            </PaperContainer>
+          </Grid>
+        </Grid>
+      </Content>
+
+      <Footer>
+        Footer do checkout
+      </Footer>
+    </>
+  )
+}
+
+function TextField ({ xs, autoFocus, ...props }) {
+  return (
+    <Grid item xs={xs}>
+      <MaterialTextField
+        fullWidth
+        variant='outlined'
+        inputProps={{
+          autoFocus
+        }}
+        {...props}
+      />
+    </Grid>
+  )
+}
+
+TextField.propTypes = {
+  autoFocus: t.bool,
+  xs: t.number
+}
+
+const Title = styled(UiTitle).attrs({
+  variant: 'h6'
+})`
+  && {
+    text-align: left;
+  }
+`
+
+const PaperContainer = styled(Paper)`
+  && {
+    flex-grow: 1;
+    margin-bottom: ${({ theme }) => theme.spacing(5)}px;
+    padding: ${({ theme }) => theme.spacing(2)}px;
+  }
+`
+
+export default Checkout
---
+++ examples/m04/04-reactzzaria/93/sistema-pedidos/src/pages/main/main.js
@@ -0,0 +1,60 @@
+import React, { Suspense } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import { withStyles } from '@material-ui/core'
+import Header from './header'
+
+import * as routes from 'routes'
+
+const ChoosePizzaSize = React.lazy(
+  () => import('pages/choose-pizza-size')
+)
+const ChoosePizzaFlavours = React.lazy(
+  () => import('pages/choose-pizza-flavours')
+)
+const ChoosePizzaQuantity = React.lazy(
+  () => import('pages/choose-pizza-quantity')
+)
+const Checkout = React.lazy(
+  () => import('pages/checkout')
+)
+
+const Main = () => (
+  <>
+    <Header />
+
+    <Spacer />
+
+    <Suspense fallback='Loading...'>
+      <Switch>
+        <Route
+          path={routes.HOME}
+          exact
+          component={ChoosePizzaSize}
+        />
+        <Route
+          path={routes.CHOOSE_PIZZA_FLAVOURS}
+          component={ChoosePizzaFlavours}
+        />
+        <Route
+          path={routes.CHOOSE_PIZZA_QUANTITY}
+          component={ChoosePizzaQuantity}
+        />
+
+        <Route
+          path={routes.CHECKOUT}
+          component={Checkout}
+        />
+      </Switch>
+    </Suspense>
+  </>
+)
+
+const style = (theme) => ({
+  main: theme.mixins.toolbar
+})
+
+const Spacer = withStyles(style)(({ classes }) => (
+  <div className={classes.main} />
+))
+
+export default Main
