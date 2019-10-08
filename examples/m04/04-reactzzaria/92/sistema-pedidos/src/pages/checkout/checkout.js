cb864783a2fea2ba5b06ed6af5990a503faaf32c
Fernando Daciuk <f.daciuk@gmail.com>
2019-10-08 16:21:09 -0300

    Reactzzaria: Add specific footer to checkout page

--- examples/m04/04-reactzzaria/92/sistema-pedidos/src/pages/checkout/checkout.js
+++ examples/m04/04-reactzzaria/92/sistema-pedidos/src/pages/checkout/checkout.js
@@ -11,6 +11,7 @@ import {
 } from '@material-ui/core'
 import {
   Content,
+  Footer,
   Title as UiTitle
 } from 'ui'
 import { useOrder } from 'hooks'
@@ -21,59 +22,65 @@ function Checkout () {
   console.log('order:', order)
 
   return (
-    <Content>
-      <Grid container spacing={4}>
-        <Grid item xs={12} md={6}>
-          <Title>Qual o endereço para entrega?</Title>
-          <PaperContainer>
-            <Grid container spacing={2}>
-              <TextField label='CEP' xs={4} autoFocus />
-              <Grid item xs={8} />
+    <>
+      <Content>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={6}>
+            <Title>Qual o endereço para entrega?</Title>
+            <PaperContainer>
+              <Grid container spacing={2}>
+                <TextField label='CEP' xs={4} autoFocus />
+                <Grid item xs={8} />
 
-              <TextField label='Rua' xs={9} />
-              <TextField label='Número' xs={3} />
-              <TextField label='Complemento' xs={12} />
-              <TextField label='Cidade' xs={9} />
-              <TextField label='Estado' xs={3} />
-            </Grid>
-          </PaperContainer>
+                <TextField label='Rua' xs={9} />
+                <TextField label='Número' xs={3} />
+                <TextField label='Complemento' xs={12} />
+                <TextField label='Cidade' xs={9} />
+                <TextField label='Estado' xs={3} />
+              </Grid>
+            </PaperContainer>
 
-          <Title>Qual o seu telefone?</Title>
-          <PaperContainer>
-            <TextField label='Telefone' xs={4} />
-          </PaperContainer>
+            <Title>Qual o seu telefone?</Title>
+            <PaperContainer>
+              <TextField label='Telefone' xs={4} />
+            </PaperContainer>
+          </Grid>
+
+          <Grid container item xs={12} md={6} direction='column'>
+            <Title>Informações do seu pedido:</Title>
+            <PaperContainer>
+              <List>
+                {order.pizzas.map((pizza, index) => {
+                  const { pizzaFlavours, pizzaSize, quantity } = pizza
+                  const { name, slices, flavours } = pizzaSize
+
+                  return (
+                    <ListItem key={index}>
+                      <Typography>
+                        {quantity} {' '}
+                        {singularOrPlural(quantity, 'pizza', 'pizzas')} {' '}
+                        <b>{name.toUpperCase()}</b> {'- '}
+                        ({slices} {singularOrPlural(slices, 'fatia', 'fatias')}, {' '}
+                        {flavours} {singularOrPlural(flavours, 'sabor', 'sabores')})
+
+                        <br />
+
+                        {singularOrPlural(pizzaFlavours.length, 'no sabor', 'nos sabores')}{' '}
+                        <b>{pizzaFlavours.map(({ name }) => name).join(', ')}</b>
+                      </Typography>
+                    </ListItem>
+                  )
+                })}
+              </List>
+            </PaperContainer>
+          </Grid>
         </Grid>
+      </Content>
 
-        <Grid container item xs={12} md={6} direction='column'>
-          <Title>Informações do seu pedido:</Title>
-          <PaperContainer>
-            <List>
-              {order.pizzas.map((pizza, index) => {
-                const { pizzaFlavours, pizzaSize, quantity } = pizza
-                const { name, slices, flavours } = pizzaSize
-
-                return (
-                  <ListItem key={index}>
-                    <Typography>
-                      {quantity} {' '}
-                      {singularOrPlural(quantity, 'pizza', 'pizzas')} {' '}
-                      <b>{name.toUpperCase()}</b> {'- '}
-                      ({slices} {singularOrPlural(slices, 'fatia', 'fatias')}, {' '}
-                      {flavours} {singularOrPlural(flavours, 'sabor', 'sabores')})
-
-                      <br />
-
-                      {singularOrPlural(pizzaFlavours.length, 'no sabor', 'nos sabores')}{' '}
-                      <b>{pizzaFlavours.map(({ name }) => name).join(', ')}</b>
-                    </Typography>
-                  </ListItem>
-                )
-              })}
-            </List>
-          </PaperContainer>
-        </Grid>
-      </Grid>
-    </Content>
+      <Footer>
+        Footer do checkout
+      </Footer>
+    </>
   )
 }
 
