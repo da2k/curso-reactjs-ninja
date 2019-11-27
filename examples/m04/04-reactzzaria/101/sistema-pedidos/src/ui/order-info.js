cb088b16de56202bd6a5f0aa010821294ad53d75
Fernando Daciuk <f.daciuk@gmail.com>
2019-11-27 10:59:35 -0300

    Reactzzaria: Add 101

---
+++ examples/m04/04-reactzzaria/101/sistema-pedidos/src/ui/order-info.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import styled from 'styled-components'
+import {
+  IconButton,
+  List,
+  ListItem as MaterialListItem,
+  Typography
+} from '@material-ui/core'
+import { Close } from '@material-ui/icons'
+import { useOrder } from 'hooks'
+import { singularOrPlural } from 'utils'
+
+function OrderInfo () {
+  const { order } = useOrder()
+
+  return (
+    <List>
+      {order.pizzas.map((pizza, index) => {
+        const { pizzaFlavours, pizzaSize, quantity } = pizza
+        const { name, slices, flavours } = pizzaSize
+
+        return (
+          <ListItem key={index}>
+            <Typography>
+              <b>{quantity}</b> {' '}
+              {singularOrPlural(quantity, 'pizza', 'pizzas')} {' '}
+              <b>{name.toUpperCase()}</b> {'- '}
+              ({slices} {singularOrPlural(slices, 'fatia', 'fatias')}, {' '}
+              {flavours} {singularOrPlural(flavours, 'sabor', 'sabores')})
+
+              <br />
+
+              {singularOrPlural(pizzaFlavours.length, 'no sabor', 'nos sabores')}{' '}
+              <b>{pizzaFlavours.map(({ name }) => name).join(', ')}</b>
+            </Typography>
+
+            <IconButton title='Remover' color='secondary'>
+              <Close />
+            </IconButton>
+          </ListItem>
+        )
+      })}
+    </List>
+  )
+}
+
+const ListItem = styled(MaterialListItem)`
+  && {
+    display: flex;
+    justify-content: space-between;
+  }
+`
+
+export default OrderInfo
