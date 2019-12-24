347f9793474ad936e87f19f3fa1d633b845f35ba
Fernando Daciuk <f.daciuk@gmail.com>
2019-12-24 18:19:52 -0300

    Reactzzaria: First update of address state using useReducer

--- examples/m04/04-reactzzaria/107/sistema-pedidos/src/pages/checkout/form-address.js
+++ examples/m04/04-reactzzaria/107/sistema-pedidos/src/pages/checkout/form-address.js
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useReducer } from 'react'
 import { Grid } from '@material-ui/core'
 import TextField from './text-field'
 
 function FormAddress () {
   const [cep, setCep] = useState('')
+  const [addressState, dispatch] = useReducer(reducer, initialState)
+
+  console.log('addressState:', addressState)
 
   useEffect(() => {
     async function fetchAddress () {
@@ -18,6 +21,11 @@ function FormAddress () {
       )
       const result = await data.json()
       console.log(result)
+
+      dispatch({
+        type: 'UPDATE_FULL_ADDRESS',
+        payload: result
+      })
     }
 
     fetchAddress()
@@ -52,6 +60,29 @@ function FormAddress () {
       <TextField label='Estado' xs={3} />
     </Grid>
   )
+}
+
+function reducer (state, action) {
+  console.log('action:', action)
+  if (action.type === 'UPDATE_FULL_ADDRESS') {
+    return {
+      ...state,
+      ...action.payload
+    }
+  }
+
+  return state
+}
+
+const initialState = {
+  code: '',
+  address: '',
+  number: '',
+  district: '',
+  complement: '',
+  city: '',
+  state: '',
+  error: null
 }
 
 export default FormAddress
