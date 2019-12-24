ead3ce87c8c4aafdbbf27cdd745bf9473d8ac41f
Fernando Daciuk <f.daciuk@gmail.com>
2019-12-24 17:22:03 -0300

    Reactzzaria: mask for CEP field

--- examples/m04/04-reactzzaria/105/sistema-pedidos/src/pages/checkout/form-address.js
+++ examples/m04/04-reactzzaria/105/sistema-pedidos/src/pages/checkout/form-address.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid } from '@material-ui/core'
 import TextField from './text-field'
 
 function FormAddress () {
+  const [cep, setCep] = useState('')
+  console.log(cep)
+
+  function handleChangeCep (e) {
+    setCep(cepMask(e.target.value))
+  }
+
+  function cepMask (value) {
+    return value
+      .replace(/\D+/g, '')
+      .replace(/(\d{5})(\d)/, '$1-$2')
+      .replace(/(-\d{3})\d+?$/, '$1')
+  }
+
   return (
     <Grid container spacing={2}>
-      <TextField label='CEP' xs={4} autoFocus />
+      <TextField
+        label='CEP'
+        xs={4}
+        autoFocus
+        value={cep}
+        onChange={handleChangeCep}
+      />
       <Grid item xs={8} />
 
       <TextField label='Rua' xs={9} />
