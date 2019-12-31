03d7f2943d8d66a33c7c9a34b26107259482b638
Fernando Daciuk <f.daciuk@gmail.com>
2019-12-31 18:03:53 -0300

    Reactzzaria: Save order on Firestore

--- examples/m04/04-reactzzaria/117/sistema-pedidos/src/contexts/order.js
+++ examples/m04/04-reactzzaria/117/sistema-pedidos/src/contexts/order.js
@@ -1,6 +1,8 @@
 import React, { createContext, useState } from 'react'
 import t from 'prop-types'
 import uuidv4 from 'uuid/v4'
+import firebase, { db } from 'services/firebase'
+import { useAuth } from 'hooks'
 
 const OrderContext = createContext()
 
@@ -9,6 +11,7 @@ function OrderProvider ({ children }) {
   const [orderInProgress, setOrderInProgress] = useState(false)
   const [phone, addPhone] = useState('')
   const [address, addAddress] = useState({})
+  const { userInfo } = useAuth()
 
   function addPizzaToOrder (pizza) {
     if (orderInProgress) {
@@ -31,8 +34,26 @@ function OrderProvider ({ children }) {
     addPizza((pizzas) => pizzas.filter(p => p.id !== id))
   }
 
-  function sendOrder () {
+  async function sendOrder () {
     console.log('send order')
+
+    try {
+      await db.collection('orders').add({
+        userId: userInfo.user.uid,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+
+        address,
+        phone,
+        pizzas: pizzas.map(pizza => ({
+          size: pizza.pizzaSize,
+          flavours: pizza.pizzaFlavours,
+          quantity: pizza.quantity
+        }))
+      })
+    } catch (e) {
+      console.log('erro ao salvar pedido:', e)
+    }
+
     setOrderInProgress(false)
   }
 
