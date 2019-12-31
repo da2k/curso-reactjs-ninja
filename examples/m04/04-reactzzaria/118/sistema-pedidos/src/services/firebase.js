b7989c4958f96330f01c12567e6ccf1c9c34ca8b
Fernando Daciuk <f.daciuk@gmail.com>
2019-12-31 19:00:22 -0300

    First version of orders app is finished

--- examples/m04/04-reactzzaria/118/sistema-pedidos/src/services/firebase.js
+++ examples/m04/04-reactzzaria/118/sistema-pedidos/src/services/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 const config = {
-  apiKey: 'AIzaSyADubq8Pf-_nC6cM52PGZJLAJ_yT4UkWto',
-  authDomain: 'reactzzaria-12649.firebaseapp.com',
-  databaseURL: 'https://reactzzaria-12649.firebaseio.com',
-  projectId: 'reactzzaria-12649',
-  storageBucket: 'reactzzaria-12649.appspot.com',
-  messagingSenderId: '190402590347'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 }
 
 firebase.initializeApp(config)
