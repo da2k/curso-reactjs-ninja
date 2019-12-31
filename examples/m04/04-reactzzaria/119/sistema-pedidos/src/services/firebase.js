65944012ff618e08f09e6c4f20e636ab43d498ae
Fernando Daciuk <f.daciuk@gmail.com>
2019-12-31 20:58:49 -0300

    Update project

--- examples/m04/04-reactzzaria/119/sistema-pedidos/src/services/firebase.js
+++ examples/m04/04-reactzzaria/119/sistema-pedidos/src/services/firebase.js
@@ -8,7 +8,8 @@ const config = {
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 }
 
 firebase.initializeApp(config)
