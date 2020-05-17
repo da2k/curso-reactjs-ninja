f5aec0f1f8480236e1f284622373e9cd797f4533
Fernando Daciuk <f.daciuk@gmail.com>
2020-05-17 15:31:11 -0300

    Reactzzaria: Add menu itens inside drawer

--- examples/m04/04-reactzzaria/123/sistema-cadastro/src/pages/main/main.js
+++ examples/m04/04-reactzzaria/123/sistema-cadastro/src/pages/main/main.js
@@ -1,12 +1,37 @@
 import React, { Suspense } from 'react'
 import styled from 'styled-components'
 import { Switch, Route } from 'react-router-dom'
-import { Drawer as MaterialDrawer } from '@material-ui/core'
+import {
+  Divider,
+  Drawer as MaterialDrawer,
+  List,
+  ListItem,
+  ListItemText,
+  Typography
+} from '@material-ui/core'
 
 const Main = () => (
   <>
     <Drawer variant='permanent'>
-      drawer
+      <DrawerContent>
+        <Typography variant='h4'>
+          React-zzaria
+        </Typography>
+
+        <Typography>
+          (sistema de cadastro)
+        </Typography>
+      </DrawerContent>
+
+      <Divider />
+
+      <List>
+        {menuItems.map(item => (
+          <ListItem key={item.label} button>
+            <ListItemText>{item.label}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
     </Drawer>
 
     <Content>
@@ -19,12 +44,33 @@ const Main = () => (
   </>
 )
 
+const menuItems = [
+  {
+    label: 'Pedidos'
+  },
+
+  {
+    label: 'Tamanhos de pizzas'
+  },
+
+  {
+    label: 'Sabores de pizzas'
+  }
+]
+
 const Drawer = styled(MaterialDrawer)`
   && {
     .MuiPaper-root {
       width: ${({ theme }) => theme.extend.drawerWidth}px;
     }
   }
+`
+
+const DrawerContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: ${({ theme }) => theme.spacing(1)}px;
+  text-align: center;
 `
 
 const Content = styled.main`
