15455cf4db25de24a85c8fbe65fe20e3874c36f3
Fernando Daciuk <f.daciuk@gmail.com>
2020-05-24 18:26:18 -0300

    Reactzzaria: Add 136

---
+++ examples/m04/04-reactzzaria/136/sistema-cadastro/src/ui/index.js
@@ -0,0 +1,8 @@
+export { default as CardLink } from './card-link'
+export { default as Content } from './content'
+export { default as Divider } from './divider'
+export { default as Footer } from './footer/footer'
+export { default as HeaderContent } from './header-content'
+export { default as OrderInfo } from './order-info'
+export { default as PizzasGrid } from './pizzas-grid'
+export { default as Title, H3, H4, H6 } from './title'
