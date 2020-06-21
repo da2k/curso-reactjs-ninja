cdce8b47dd60ed19d666fb766aab4e642a66dc44
Fernando Daciuk <f.daciuk@gmail.com>
2020-06-21 16:49:01 -0300

    Reactzzaria: Add 149

---
+++ examples/m04/04-reactzzaria/149/sistema-cadastro/src/hooks/db/collection.js
@@ -0,0 +1,50 @@
+import { useEffect, useState, useCallback } from 'react'
+import { useLocation } from 'react-router-dom'
+import { db } from 'services/firebase'
+import { useMounted } from 'hooks'
+
+function useCollection (collection) {
+  const [data, setData] = useState(null)
+  const { pathname } = useLocation()
+  const mounted = useMounted()
+
+  const fetchCollectionData = useCallback(() => {
+    db.collection(collection).get().then(querySnapshot => {
+      const docs = []
+
+      querySnapshot.forEach(doc => {
+        docs.push({
+          id: doc.id,
+          ...doc.data()
+        })
+      })
+
+      if (mounted.current) {
+        setData(docs)
+      }
+    })
+  }, [collection, mounted])
+
+  const add = useCallback((data) => {
+    console.log('data add new', data)
+    return db.collection(collection).add(data)
+  }, [collection])
+
+  const edit = useCallback((id, data) => {
+    console.log('edit:', id, data)
+    return db.collection(collection).doc(id).set(data)
+  }, [collection])
+
+  const remove = useCallback(async (id) => {
+    await db.collection(collection).doc(id).delete()
+    fetchCollectionData()
+  }, [collection, fetchCollectionData])
+
+  useEffect(() => {
+    fetchCollectionData()
+  }, [pathname, fetchCollectionData])
+
+  return { data, add, edit, remove }
+}
+
+export default useCollection
---
+++ examples/m04/04-reactzzaria/149/sistema-cadastro/src/routes.js
@@ -0,0 +1,8 @@
+export const HOME = '/'
+export const LOGIN = '/login'
+export const PIZZAS_SIZES = '/tamanhos'
+export const PIZZAS_FLAVOURS = '/sabores'
+
+// actions
+export const NEW = '/novo'
+export const EDIT = (id = ':id') => `/${id}/editar`
