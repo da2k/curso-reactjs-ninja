cbb72a42fc910e209c38f9575d985eabc81441f2
Fernando Daciuk <f.daciuk@gmail.com>
2020-07-25 17:55:27 -0300

    01-hello-react-router BrowserRouter, Link and Route

--- examples/m04/01-hello-react-router/01/src/App.js
+++ examples/m04/01-hello-react-router/01/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Link, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div style={{ padding: 20 }}>
+        <Route path='/sobre' component={Sobre} />
+        <Link to='/sobre'>Sobre</Link>
+      </div>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+const Sobre = () => (
+  <h1>Sobre</h1>
+)
+
+export default App
